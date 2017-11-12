4650851fabb307ae1d0e0efa92314ee7c1117a8b
Amund Tenstad <github@amund.io>
2017-11-12 12:38:35 +0100

    Create temp page

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from web.views import IndexView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', IndexView.as_view()),
 ]
