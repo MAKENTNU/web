ca7fcd28d11aa41f10bc70e7f805ad356d93065a
TommyWithATiger <halvard.hummel@hotmail.com>
2017-11-17 11:46:21 +0100

    Added a test to check that a valid reservation is actually validated correctly

--- make_queue/tests.py
+++ make_queue/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from make_queue.models import Printer3D, Reservation3D, Quota3D
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta
 
-# Create your tests here.
+
+class Reservation3DTestCase(TestCase):
+    def setUp(self):
+        Printer3D.objects.create(name="C1", location="Printer room Makerspace U1", status="F")
+        user = User.objects.create_user("User", "user@makentnu.no", "user_pass")
+        user.save()
+        Quota3D.objects.create(user=user, can_print=True, max_time_reservation=10, max_number_of_reservations=2)
+
+    def test_can_create_reservation(self):
+        printer = Printer3D.objects.get(name="C1")
+        user = User.objects.get(username="User")
+
+        reservation = Reservation3D(user=user, printer=printer,
+                                    start_time=datetime.now(), end_time=datetime.now() + timedelta(hours=2),
+                                    event=False)
+
+        self.assertTrue(reservation.validate())
+        try:
+            reservation.save()
+        except ValidationError:
+            self.fail("Could not save a valid reservation")
+            
