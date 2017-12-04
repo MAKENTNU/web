ae7d6eaa6dce924f9db8bf32e8a5f3e7bfbfcf19
Amund Tenstad <github@amund.io>
2017-12-04 09:49:16 +0100

    Setup dataporten

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from web.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', IndexView.as_view()),
+    url(r'^$', IndexView.as_view()),
+    url(r'', include('social_django.urls', namespace='social')),
 ]
