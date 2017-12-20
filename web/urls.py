27d9ab14dafa6c447f4f9f28b49b7d6e3b3fc6ac
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-20 16:10:10 +0100

    Added the make queue urls to the main url config

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from web.views import IndexView
 
 urlpatterns = [
+    url(r'^reservation/', include('make_queue.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', IndexView.as_view()),
 ]
