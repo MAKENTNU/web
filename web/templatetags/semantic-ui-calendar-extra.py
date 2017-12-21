1567b9bd7c83e70f40010f14f3e252085f8ea688
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-21 22:26:36 +0100

    The get current date template tag now allows for a shift to be added

--- web/templatetags/semantic-ui-calendar-extra.py
+++ web/templatetags/semantic-ui-calendar-extra.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 
 @register.simple_tag()
-def set_current_date():
-    return timezone.now().strftime("%m/%d/%Y %H:%M")
+def set_current_date(shift_hours=0):
+    return (timezone.now() + timezone.timedelta(hours=shift_hours)).strftime("%m/%d/%Y %H:%M")
