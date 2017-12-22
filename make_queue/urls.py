09ad0e16b0beefdef099422f2a7df5dfd090ab24
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-22 16:28:55 +0100

    Removed zero padding from number in make reservation url and changed from machine name to pk

--- make_queue/urls.py
+++ make_queue/urls.py
@@ -4,5 +4,5 @@ from make_queue.views import ReservationCalendarView, MakeReservationView
 
 urlpatterns = [
     url('^(?P<year>[0-9]{4})/(?P<week>([0-9]|[1-4][0-9]|5[0-2]))/(?P<machine_type>[a-zA-Z0-9-]+)$', ReservationCalendarView.as_view()),
-    url('^make/((?P<start_time>(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/([0-9]{4})/([01][0-9]|2[0-3]):([0-5][0-9]))/)?((?P<selected_machine_type>[a-zA-Z0-9-]+)/((?P<selected_machine_name>[a-zA-Z0-9-]+)/)?)?$', MakeReservationView.as_view()),
+    url('^make/((?P<start_time>([1-9]|1[0-2])/([1-9]|[12][0-9]|3[01])/([0-9]{4})/([01][0-9]|2[0-3]):([0-5][0-9]))/)?((?P<selected_machine_type>[a-zA-Z0-9-]+)/((?P<selected_machine_pk>[0-9]+)/)?)?$', MakeReservationView.as_view()),
 ]
