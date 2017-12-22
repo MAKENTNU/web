81249dac092030bdf9744df3adb9d84e9e5ff462
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-22 16:30:55 +0100

    Clicking a machine on a specific day now takes the user to the create a reservation page for that machine

--- make_queue/static/make_queue/js/reservation_calendar.js
+++ make_queue/static/make_queue/js/reservation_calendar.js
@@ -1,5 +1,12 @@
 $(".make_reservation_calendar_time_table").click(function (event) {
-    // TODO: Add linking to the create reservation page with the correct data
+    let clicked_ratio = (event.pageY - $(this).offset().top)/$(this).height();
+    let hours = ("00" + Math.floor(clicked_ratio*24)).substr(-2, 2);
+    let minutes = ("00" + Math.floor((clicked_ratio*24%1)*60)).substr(-2, 2);
+    let data_element = $(this).closest(".make_reservation_calendar_time_table");
+    let date = data_element.data("date");
+    let machine_type = data_element.data("machine-type");
+    let machine_pk = data_element.data("machine");
+    document.location = "/reservation/make/"+date+"/"+hours+":"+minutes+"/"+machine_type+"/"+machine_pk+"/";
 });
 
 $(".make_reservation_calendar_time_table_item").popup({on: "hover", position: "right center"});
