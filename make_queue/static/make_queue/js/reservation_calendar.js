988ce5adff9ea28e9cae8d9c6386f0c2acdaa7b4
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-22 11:40:55 +0100

    Added a popup for each event in the reservation calendar

--- make_queue/static/make_queue/js/reservation_calendar.js
+++ make_queue/static/make_queue/js/reservation_calendar.js
@@ -1,3 +1,5 @@
 $(".make_reservation_calendar_time_table").click(function (event) {
     // TODO: Add linking to the create reservation page with the correct data
 });
+
+$(".make_reservation_calendar_time_table_item").popup({on: "hover", position: "right center"});
