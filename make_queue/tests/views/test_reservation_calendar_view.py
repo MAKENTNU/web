73dbb93523d3b8d585b5652c8727c621567f0293
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-24 12:56:54 +0100

    Added tests for the is_valid_week method

--- make_queue/tests/views/test_reservation_calendar_view.py
+++ make_queue/tests/views/test_reservation_calendar_view.py
@@ -8,3 +8,12 @@ class ReservationCalendarViewTestCase(TestCase):
     def test_year_and_week_to_monday(self):
         date = datetime(2017, 12, 18)
         self.assertEqual(date, ReservationCalendarView.year_and_week_to_monday(2017, 51))
+
+    def test_is_valid_week_with_zero_week_number(self):
+        self.assertFalse(ReservationCalendarView.is_valid_week(2017, 0))
+
+    def test_is_valid_week_with_valid_week(self):
+        self.assertTrue(ReservationCalendarView.is_valid_week(2017, 51))
+
+    def test_is_valid_week_with_to_high_week(self):
+        self.assertFalse(ReservationCalendarView.is_valid_week(2017, 53))
