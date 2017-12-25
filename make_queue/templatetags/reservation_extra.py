eb9086d3a79c4dab2d199c2cb712d3498c0b31ff
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-25 18:55:52 +0100

    Create a couple of templatetags for reservations

---
+++ make_queue/templatetags/reservation_extra.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils import timezone
+from django.urls import reverse
+
+register = template.Library()
+
+
+@register.simple_tag()
+def can_delete_reservation(reservation):
+    return timezone.now() < reservation.end_time
+
+
+@register.simple_tag()
+def calendar_url_reservation(reservation):
+    return reverse('reservation_calendar',
+                   kwargs={'year': reservation.start_time.year, 'week': reservation.start_time.isocalendar()[1],
+                           'machine_type': reservation.machine.literal})
