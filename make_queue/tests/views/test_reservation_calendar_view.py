112f330c5ac9915fb489aaef0ec097ddb6891fcc
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-25 15:22:21 +0100

    Added tests for the reservation format method

--- make_queue/tests/views/test_reservation_calendar_view.py
+++ make_queue/tests/views/test_reservation_calendar_view.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from datetime import datetime
+from django.utils import timezone
+import pytz
 from make_queue.views import ReservationCalendarView
-from make_queue.models import Printer3D, SewingMachine
+from make_queue.models import Printer3D, SewingMachine, Reservation3D, Quota3D
 
 
 class ReservationCalendarViewTestCase(TestCase):
@@ -50,3 +53,49 @@ class ReservationCalendarViewTestCase(TestCase):
         Printer3D.objects.create(name="S1", location="U1", model="Ultimaker", status="F")
         Printer3D.objects.create(name="S2", location="U1", model="Ultimaker", status="F")
         self.assertEqual(list(Printer3D.objects.all()), list(ReservationCalendarView.get_machines(Printer3D.literal)))
+
+    @staticmethod
+    def create_reservation(start_time, end_time):
+        Printer3D.objects.create(name="S1", location="U1", model="Ultimaker", status="F")
+        user = User.objects.create_user("User", "user@makentnu.no", "unsecure_pass")
+        user.save()
+        Quota3D.objects.create(user=user, max_time_reservation=40, max_number_of_reservations=2, can_print=True)
+        Reservation3D.objects.create(user=user, machine=Printer3D.objects.get(name="S1"),
+                                     start_time=pytz.timezone(timezone.get_default_timezone_name()).localize(
+                                         start_time),
+                                     end_time=pytz.timezone(timezone.get_default_timezone_name()).localize(end_time),
+                                     event=False)
+        return Reservation3D.objects.first()
+
+    def test_format_reservation_start_end_same_day(self):
+        reservation = self.create_reservation(datetime(2017, 12, 25, 12), datetime(2017, 12, 25, 18))
+        self.assertEqual(ReservationCalendarView.format_reservation(reservation, pytz.timezone(
+            timezone.get_default_timezone_name()).localize(datetime(2017, 12, 25))), {
+                             'reservation': reservation,
+                             'start_percentage': 50,
+                             'start_time': "12:00",
+                             'end_time': "18:00",
+                             'length': 25
+                         })
+
+    def test_format_reservation_start_day_before(self):
+        reservation = self.create_reservation(datetime(2017, 12, 24, 12), datetime(2017, 12, 25, 6))
+        self.assertEqual(ReservationCalendarView.format_reservation(reservation, pytz.timezone(
+            timezone.get_default_timezone_name()).localize(datetime(2017, 12, 25))), {
+                             'reservation': reservation,
+                             'start_percentage': 0,
+                             'start_time': "00:00",
+                             'end_time': "06:00",
+                             "length": 25
+                         })
+
+    def test_format_reservation_end_day_after(self):
+        reservation = self.create_reservation(datetime(2017, 12, 25, 12), datetime(2017, 12, 26, 3))
+        self.assertEqual(ReservationCalendarView.format_reservation(reservation, pytz.timezone(
+            timezone.get_default_timezone_name()).localize(datetime(2017, 12, 25))), {
+                             'reservation': reservation,
+                             'start_percentage': 50,
+                             'start_time': '12:00',
+                             'end_time': "23:59",
+                             'length': 50 - 100 / 1440
+                         })
