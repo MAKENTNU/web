43d35b833be3808d6c54526baee8ef0e2484f19d
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-25 23:51:02 +0100

    The can delete template tag is now irrelevant

--- make_queue/templatetags/reservation_extra.py
+++ make_queue/templatetags/reservation_extra.py
@@ -6,11 +6,6 @@ register = template.Library()
 
 
 @register.simple_tag()
-def can_delete_reservation(reservation):
-    return timezone.now() < reservation.end_time
-
-
-@register.simple_tag()
 def calendar_url_reservation(reservation):
     return reverse('reservation_calendar',
                    kwargs={'year': reservation.start_time.year, 'week': reservation.start_time.isocalendar()[1],
