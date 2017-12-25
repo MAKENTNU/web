b77da604e4bea4cd2a621768b6efc1d354e55bd1
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-25 17:12:27 +0100

    The make reservation page should require login

--- make_queue/urls.py
+++ make_queue/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from make_queue.views import ReservationCalendarView, MakeReservationView
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     url('^(?P<year>[0-9]{4})/(?P<week>([0-9]|[1-4][0-9]|5[0-3]))/(?P<machine_type>[a-zA-Z0-9-]+)$', ReservationCalendarView.as_view()),
-    url('^make/((?P<start_time>([1-9]|1[0-2])/([1-9]|[12][0-9]|3[01])/([0-9]{4})/([01][0-9]|2[0-3]):([0-5][0-9]))/)?((?P<selected_machine_type>[a-zA-Z0-9-]+)/((?P<selected_machine_pk>[0-9]+)/)?)?$', MakeReservationView.as_view()),
+    url('^make/((?P<start_time>([1-9]|1[0-2])/([1-9]|[12][0-9]|3[01])/([0-9]{4})/([01][0-9]|2[0-3]):([0-5][0-9]))/)?((?P<selected_machine_type>[a-zA-Z0-9-]+)/((?P<selected_machine_pk>[0-9]+)/)?)?$', login_required(MakeReservationView.as_view())),
 ]
