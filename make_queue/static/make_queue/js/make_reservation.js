a67baf2b0229b29204aecae9ff1e6e6b5b9721f2
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-28 16:23:55 +0100

    Fixed the input value of the start and end time at submit

--- make_queue/static/make_queue/js/make_reservation.js
+++ make_queue/static/make_queue/js/make_reservation.js
@@ -1,30 +1,42 @@
-$("#start_date").calendar({
+$("#start_time").calendar({
         minDate: new Date(),
         ampm: false,
-        endCalendar: $("#end_date"),
+        endCalendar: $("#end_time"),
         initialDate: null,
     }
 );
 
-$("#end_date").calendar({
-        ampm: false,
-        startCalendar: $("#start_date")
+$("#end_time").calendar({
+    ampm: false,
+    startCalendar: $("#start_time"),
 });
 
 $('.ui.dropdown').dropdown();
 $('#event_checkbox').checkbox({
-    onChange: function() {
+    onChange: function () {
         $("#event_name_input").toggleClass("make_non_visible", !$(this).is(':checked'))
     },
 });
 
-$('#machine_type_dropdown').dropdown('setting', 'onChange', function(value) {
+$('#machine_type_dropdown').dropdown('setting', 'onChange', function (value) {
     $('#machine_name_dropdown').toggleClass("disabled", false).dropdown("restore defaults");
     $('#machine_name_dropdown .menu .item').toggleClass("make_hidden", true);
     $('#machine_name_dropdown .menu').toggleClass("menu", false);
-    $('#machine_name_dropdown .'+value).toggleClass("menu", true);
+    $('#machine_name_dropdown .' + value).toggleClass("menu", true);
     $('#machine_name_dropdown .menu .item').toggleClass("make_hidden", false);
 });
 
 $('#machine_type_dropdown').dropdown("set selected", $('.selected_machine_type').data("value"));
 $('#machine_name_dropdown').dropdown("set selected", $('.selected_machine_name').data("value"));
+
+zeroPadDateElement = (val) => val < 10 ? "0" + val : val;
+
+function formatDate(date) {
+    return date.getFullYear() + "-" + zeroPadDateElement(date.getMonth() + 1) + "-" + zeroPadDateElement(date.getDate())
+        + " " + zeroPadDateElement(date.getHours()) + ":" + zeroPadDateElement(date.getMinutes())
+}
+
+$('form').submit(function () {
+    $("#start_time input").first().val(formatDate($("#start_time").calendar("get date")));
+    $("#end_time input").first().val(formatDate($("#end_time").calendar("get date")));
+});
