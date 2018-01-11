a401b93c41d82694c13f83ee105dc76a07c498c5
Amund Tenstad <github@amund.io>
2018-01-11 07:47:03 +0100

    Create create/edit view/form

--- news/views.py
+++ news/views.py
@@ -1,1 +1,73 @@
-from django.shortcuts import render
+from django.views.generic import UpdateView, CreateView
+
+from news.models import Article, Event
+
+
+class EditArticleView(UpdateView):
+    model = Article
+    template_name = 'news/article_edit.html'
+    fields = (
+        'title',
+        'content',
+        'clickbait',
+        'image',
+        'hidden',
+        'private',
+    )
+    success_url = '/'
+
+
+class CreateArticleView(CreateView):
+    model = Article
+    template_name = 'news/article_create.html'
+    fields = (
+        'title',
+        'content',
+        'clickbait',
+        'image',
+        'hidden',
+        'private',
+    )
+    success_url = '/'
+
+
+class EditEventView(UpdateView):
+    model = Event
+    template_name = 'news/event_edit.html'
+    fields = (
+        'title',
+        'content',
+        'clickbait',
+        'image',
+        'hidden',
+        'private',
+        'start_date',
+        'end_date',
+        'start_time',
+        'end_time',
+        'place',
+        'place_url',
+        'hoopla',
+    )
+    success_url = '/'
+
+
+class CreateEventView(CreateView):
+    model = Event
+    template_name = 'news/event_create.html'
+    fields = (
+        'title',
+        'content',
+        'clickbait',
+        'image',
+        'hidden',
+        'private',
+        'start_date',
+        'end_date',
+        'start_time',
+        'end_time',
+        'place',
+        'place_url',
+        'hoopla',
+    )
+    success_url = '/'
