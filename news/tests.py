f7165429500246f739d7ca7390c0164cf02969d2
Amund Tenstad <github@amund.io>
2018-01-12 18:41:31 +0100

    Test article model

--- news/tests.py
+++ news/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+from news.models import Article
+
+
+class ModelTestCase(TestCase):
+    def test_str(self):
+        article = Article.objects.create(title='TEST_TITLE')
+        self.assertEqual(article.title, 'TEST_TITLE')
+        self.assertEqual(article.title, str(article))
