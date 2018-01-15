16bc7ac61cc6d5aef25356d6bd3dda0cd70fe387
TommyWithATiger <halvard.hummel@hotmail.com>
2018-01-15 01:56:20 +0100

    The calendar selection in the controls for the reservation timetable now redirects to the correct page

--- make_queue/static/make_queue/js/reservation_calendar.js
+++ make_queue/static/make_queue/js/reservation_calendar.js
@@ -1,3 +1,11 @@
+function getWeekNumber(date) {
+    date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+    var weekNumber = Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
+    return weekNumber;
+}
+
 $(".make_reservation_calendar_time_table_row").click(function (event) {
     let clicked_ratio = (event.pageX - $(this).offset().left) / $(this).width();
     let hours = ("00" + Math.floor(clicked_ratio * 24)).substr(-2, 2);
@@ -5,23 +13,18 @@ $(".make_reservation_calendar_time_table_row").click(function (event) {
     let date = $(this).data("date");
     let machine_type = $(this).data("machine-type");
     let machine_pk = $(this).data("machine");
-    document.location = "/reservation/make/" + date + "/" + hours + ":" + minutes + "/" + machine_type + "/" + machine_pk + "/";
+    document.location = "/reservation/make" + date + "/" + hours + ":" + minutes + "/" + machine_type + "/" + machine_pk + "/";
 });
-
-$(".make_reservation_calendar_time_table_item").popup({on: "hover", position: "right center"});
-$(".make_reservation_calendar_time_table_item").click(false);
-$("#machine_type_dropdown").dropdown().dropdown("set selected", $('.selected_machine_type').data("value"))
-    .dropdown("setting", "onChange", function (value) {
-        document.location = value;
-    });
 
 $("#period").calendar({
         ampm: false,
         initialDate: null,
         type: 'date',
-        onChange: function (value) {
-            // TODO: FIX redirect to correct day
-            console.log(value);
+        onChange: function (date) {
+            let current_location = document.location.href.split("/");
+            current_location[current_location.length - 4] = date.getFullYear();
+            current_location[current_location.length - 3] = getWeekNumber(date);
+            document.location = current_location.join("/");
         }
     }
 );
