f509c2971377382e4fc3c323760516569ed880de
Shahzeb <33691958+shahzebs@users.noreply.github.com>
2018-01-17 21:04:08 +0100

    Updated profile and skill models

--- checkin/models.py
+++ checkin/models.py
@@ -1,10 +1,22 @@
 from datetime import time
 
+from django.contrib.auth.models import User
 from django.db import models
 from ckeditor.fields import RichTextField
 from django.utils import timezone
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Navn")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
     card_id = models.CharField(max_length=100, verbose_name="Kortnummer")
+    skill = models.ManyToManyField(Skill)
+    #TODO: imagefield
+
+class Skill(models.Model):
+    level_choices = (
+        (1, "Nybegynner"),
+        (2, "Viderekommen"),
+        (3, "Ekspert"),
+    )
+    title = models.CharField(max_length=100, verbose_name="Ferdighet")
+    skill_level = models.IntegerField(choices=level_choices)
