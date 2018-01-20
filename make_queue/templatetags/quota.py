e28c47fc8e02a60b74f919abf4b366ce6e43cd2e
TommyWithATiger <halvard.hummel@hotmail.com>
2018-01-20 13:04:45 +0100

    Added a templatetag to get (and if needed) create a users 3D quota

---
+++ make_queue/templatetags/quota.py
@@ -0,0 +1,11 @@
+from django import template
+from make_queue.models import Quota3D
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_3d_quota(user):
+    if hasattr(user, "quota3d"):
+        return user.quota3d
+    return Quota3D.objects.create(user=user)
