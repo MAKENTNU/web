e4cbdcde6e3cdb8421ee565f370610104a1ca9d2
Amund Tenstad <github@amund.io>
2018-01-26 17:33:57 +0100

    Test news managers

--- news/tests.py
+++ news/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
+from datetime import timedelta
 
-from news.models import Article
+from django.test import TestCase
+from django.utils import timezone
+
+from news.models import Article, Event
 
 
 class ModelTestCase(TestCase):
@@ -8,3 +11,51 @@ class ModelTestCase(TestCase):
         article = Article.objects.create(title='TEST_TITLE')
         self.assertEqual(article.title, 'TEST_TITLE')
         self.assertEqual(article.title, str(article))
+
+    def test_article_manager(self):
+        Article.objects.create(
+            title='NOT PUBLISHED',
+            pub_date=timezone.now() + timedelta(days=1),
+            pub_time=timezone.now().time()
+        )
+        Article.objects.create(
+            title='NOT PUBLISHED',
+            pub_date=timezone.now(),
+            pub_time=(timezone.now() + timedelta(seconds=1)).time()
+        )
+        Article.objects.create(
+            title='PUBLISHED',
+            pub_date=timezone.now() - timedelta(days=1),
+            pub_time=timezone.now().time()
+        )
+        Article.objects.create(
+            title='PUBLISHED',
+            pub_date=timezone.now(),
+            pub_time=(timezone.now() - timedelta(seconds=1)).time()
+        )
+        self.assertEqual(Article.objects.published().count(), 2)
+        self.assertEqual(list(Article.objects.published().values_list('title', flat=True)), ['PUBLISHED']*2)
+
+    def test_event_manager(self):
+        Event.objects.create(
+            title='NOT PUBLISHED',
+            pub_date=timezone.now() + timedelta(days=1),
+            start_date=timezone.now(),
+            start_time=(timezone.now() + timedelta(seconds=1)).time()
+        )
+        Event.objects.create(
+            title='FUTURE',
+            pub_date=timezone.now() - timedelta(days=1),
+            start_date=timezone.now(),
+            start_time=(timezone.now() + timedelta(seconds=1)).time()
+        )
+        Event.objects.create(
+            title='PAST',
+            pub_date=timezone.now() - timedelta(days=1),
+            start_date=timezone.now(),
+            start_time=(timezone.now() - timedelta(seconds=1)).time()
+        )
+        self.assertEqual(Event.objects.future().count(), 1)
+        self.assertEqual(Event.objects.past().count(), 1)
+        self.assertEqual(Event.objects.future().first().title, 'FUTURE')
+        self.assertEqual(Event.objects.past().first().title, 'PAST')
