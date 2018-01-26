c94f71dd17bfdf24b5406ae519594938fc17b9b7
Amund Tenstad <github@amund.io>
2018-01-26 18:12:01 +0100

    Test news views

--- news/tests.py
+++ news/tests.py
@@ -1,6 +1,9 @@
 from datetime import timedelta
 
+from django.contrib.auth.models import User, Permission
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from news.models import Article, Event
@@ -34,7 +37,7 @@ class ModelTestCase(TestCase):
             pub_time=(timezone.now() - timedelta(seconds=1)).time()
         )
         self.assertEqual(Article.objects.published().count(), 2)
-        self.assertEqual(list(Article.objects.published().values_list('title', flat=True)), ['PUBLISHED']*2)
+        self.assertEqual(list(Article.objects.published().values_list('title', flat=True)), ['PUBLISHED'] * 2)
 
     def test_event_manager(self):
         Event.objects.create(
@@ -59,3 +62,73 @@ class ModelTestCase(TestCase):
         self.assertEqual(Event.objects.past().count(), 1)
         self.assertEqual(Event.objects.future().first().title, 'FUTURE')
         self.assertEqual(Event.objects.past().first().title, 'PAST')
+
+
+class ViewTestCase(TestCase):
+    def add_permission(self, codename):
+        permission = Permission.objects.get(codename=codename)
+        self.user.user_permissions.add(permission)
+
+    def setUp(self):
+        username = 'TEST_USER'
+        password = 'TEST_PASS'
+        self.user = User.objects.create_user(username=username, password=password)
+        self.client.login(username=username, password=password)
+
+        self.article = Article.objects.create(
+            title='PUBLISHED',
+            image=SimpleUploadedFile(name='img.jpg', content='', content_type='image/jpeg'),
+            pub_date=timezone.now() - timedelta(days=1),
+            pub_time=timezone.now().time()
+        )
+        self.event = Event.objects.create(
+            title='FUTURE',
+            image=SimpleUploadedFile(name='img.jpg', content='', content_type='image/jpeg'),
+            pub_date=timezone.now() - timedelta(days=1),
+            start_date=timezone.now(),
+            start_time=(timezone.now() + timedelta(seconds=1)).time()
+        )
+
+    def test_articles(self):
+        response = self.client.get(reverse('articles'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_article(self):
+        response = self.client.get(reverse('article', kwargs={'pk': self.article.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_create(self):
+        response = self.client.get(reverse('article-create'))
+        self.assertNotEqual(response.status_code, 200)
+        self.add_permission('add_article')
+        response = self.client.get(reverse('article-create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_edit(self):
+        response = self.client.get(reverse('article-edit', kwargs={'pk': self.article.pk}))
+        self.assertNotEqual(response.status_code, 200)
+        self.add_permission('change_article')
+        response = self.client.get(reverse('article-edit', kwargs={'pk': self.article.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_events(self):
+        response = self.client.get(reverse('events'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_event(self):
+        response = self.client.get(reverse('event', kwargs={'pk': self.event.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_event_create(self):
+        response = self.client.get(reverse('event-create'))
+        self.assertNotEqual(response.status_code, 200)
+        self.add_permission('add_event')
+        response = self.client.get(reverse('event-create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_event_edit(self):
+        response = self.client.get(reverse('event-edit', kwargs={'pk': self.event.pk}))
+        self.assertNotEqual(response.status_code, 200)
+        self.add_permission('change_event')
+        response = self.client.get(reverse('event-edit', kwargs={'pk': self.event.pk}))
+        self.assertEqual(response.status_code, 200)
