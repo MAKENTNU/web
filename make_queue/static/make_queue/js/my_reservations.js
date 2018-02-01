827ba1800bee696af1ebb103e57c38096eaa3ed2
TommyWithATiger <halvard.hummel@hotmail.com>
2018-02-01 17:10:23 +0100

    Checkbox for hidingold reservations in the my reservations page

---
+++ make_queue/static/make_queue/js/my_reservations.js
@@ -0,0 +1,8 @@
+$("#hide_old_reservations").checkbox({
+    onChange: function () {
+        $("tr").filter(function () {
+            console.log();
+            return $(this).data("is-future-reservation") === "False";
+        }).toggleClass("make_hidden", $(this).is(":checked"))
+    }
+});
