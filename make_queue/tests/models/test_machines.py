17c261c58a4c37797474c13db0052342d97dc7e5
zootos <halvard.hummel@hotmail.com>
2018-02-27 08:53:15 +0100

    Fixed failing tests

--- make_queue/tests/models/test_machines.py
+++ make_queue/tests/models/test_machines.py
@@ -6,10 +6,10 @@ from make_queue.models import Printer3D, SewingMachine
 class Printer3DTest(TestCase):
 
     def setUp(self):
-        self.printer = Printer3D.objects.create(name="C1", location="Printer room", status="F")
+        self.printer = Printer3D.objects.create(name="C1", location="Printer room", status="F", model="Ultimaker 2 Extended")
 
     def test_to_string(self):
-        self.assertEqual(str(self.printer), "C1 - Printer room")
+        self.assertEqual(str(self.printer), "C1 - Ultimaker 2 Extended")
 
 
 class SewingTest(TestCase):
@@ -18,4 +18,4 @@ class SewingTest(TestCase):
         self.sewing = SewingMachine.objects.create(name="C1", location="Makerspace U1", status="F", model="Generic")
 
     def test_to_string(self):
-        self.assertEqual(str(self.sewing), "C1 - Makerspace U1")
+        self.assertEqual(str(self.sewing), "C1 - Generic")
