811dd0c7398b7280da209e646a40ad4b2afae77b
zootos <halvard.hummel@hotmail.com>
2018-03-01 15:34:28 +0100

    Test for machine subclass method

--- make_queue/tests/models/test_machines.py
+++ make_queue/tests/models/test_machines.py
@@ -1,12 +1,25 @@
 from django.test import TestCase
 
-from make_queue.models import Printer3D, SewingMachine
+from make_queue.models import Printer3D, SewingMachine, Machine
+
+
+class TestGenericMachine(TestCase):
+
+    def test_get_subclass(self):
+        self.assertEquals(Machine.get_subclass(Printer3D.literal), Printer3D)
+        self.assertEquals(Machine.get_subclass(SewingMachine.literal), SewingMachine)
+        try:
+            Machine.get_subclass("")
+            self.fail("Getting a subclass that does not exist, should throw a StopIteration exception")
+        except StopIteration:
+            pass
 
 
 class Printer3DTest(TestCase):
 
     def setUp(self):
-        self.printer = Printer3D.objects.create(name="C1", location="Printer room", status="F", model="Ultimaker 2 Extended")
+        self.printer = Printer3D.objects.create(name="C1", location="Printer room", status="F",
+                                                model="Ultimaker 2 Extended")
 
     def test_to_string(self):
         self.assertEqual(str(self.printer), "C1 - Ultimaker 2 Extended")
