006c39d7985caba58d4fd76fa13d32fb64b54e46
zootos <halvard.hummel@hotmail.com>
2018-03-05 11:55:26 +0100

    Template tags tests

--- make_queue/tests/test_templatetags.py
+++ make_queue/tests/test_templatetags.py
@@ -1,10 +1,10 @@
+from datetime import timedelta
+
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.urls.base import reverse
-from django.utils import timezone
 import pytz
 import mock
-from make_queue.templatetags.reservation_extra import calendar_url_reservation, current_calendar_url
+from make_queue.templatetags.reservation_extra import *
 from make_queue.models import Printer3D, Quota3D, Reservation3D
 
 
@@ -39,3 +39,51 @@ class ReservationExtraTestCases(TestCase):
         self.assertEqual(reverse('reservation_calendar',
                                  kwargs={'year': 2017, 'week': 52, 'machine': printer}),
                          current_calendar_url(printer))
+
+    @mock.patch('django.utils.timezone.now')
+    def test_is_current_data(self, now_mock):
+        date = timezone.datetime(2017, 3, 5, 11, 18, 0)
+        now_mock.return_value = pytz.timezone(timezone.get_default_timezone_name()).localize(date)
+
+        self.assertTrue(is_current_date(timezone.now().date()))
+        self.assertTrue(is_current_date((timezone.now() + timedelta(hours=1)).date()))
+        self.assertFalse(is_current_date((timezone.now() + timedelta(days=1)).date()))
+        self.assertFalse(is_current_date((timezone.now() + timedelta(days=-1)).date()))
+
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_time_of_day(self, now_mock):
+        def set_mock_value(hours, minutes):
+            date = timezone.datetime(2017, 3, 5, hours, minutes, 0)
+            now_mock.return_value = pytz.timezone(timezone.get_default_timezone_name()).localize(date)
+
+        set_mock_value(12, 0)
+        self.assertEqual(50, get_current_time_of_day())
+
+        set_mock_value(0, 0)
+        self.assertEqual(0, get_current_time_of_day())
+
+        set_mock_value(13, 0)
+        self.assertEqual((13/24)*100, get_current_time_of_day())
+
+    def test_date_to_percentage(self):
+        date = timezone.datetime(2017, 3, 5, 12, 0, 0)
+        self.assertEqual(50, date_to_percentage(date))
+
+        date = timezone.datetime(2017, 3, 5, 0, 0, 0)
+        self.assertEqual(0, date_to_percentage(date))
+
+        date = timezone.datetime(2017, 3, 5, 17, 0, 0)
+        self.assertEqual((17/24)*100, date_to_percentage(date))
+
+        date = timezone.datetime(2017, 3, 5, 17, 25, 0)
+        self.assertEqual((17/24 + 25/1440)*100, date_to_percentage(date))
+
+    def test_invert(self):
+        self.assertEqual("true", invert(0))
+        self.assertEqual("false", invert(1))
+        self.assertEqual("true", invert(""))
+        self.assertEqual("false", invert("true"))
+        self.assertEqual("false", invert("test"))
+        self.assertEqual("true", invert(False))
+        self.assertEqual("false", invert(True))
+
