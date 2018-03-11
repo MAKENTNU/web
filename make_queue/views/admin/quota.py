cbf2a83db1ed0c0284cc2197a4213e3d17193687
zootos <halvard.hummel@hotmail.com>
2018-03-12 00:34:36 +0100

    Let the quota admin be a template view

--- make_queue/views/admin/quota.py
+++ make_queue/views/admin/quota.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 
-class QuotaView(View):
+class QuotaView(TemplateView):
+    """View for the quota admin panel that allows users to control the quotas of people"""
     template_name = "make_queue/quota_panel.html"
 
-    def get(self, request):
-        return render(request, self.template_name, {"users": User.objects.all(), "user": request.user})
+    def get_context_data(self):
+        """
+        Creates the required context for the quota panel
+
+        :return: A list of all users
+        """
+        return {"users": User.objects.all()}
