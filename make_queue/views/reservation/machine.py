6213a8fa327267b2efdfe61017ffc30c1224869b
zootos <halvard.hummel@hotmail.com>
2018-03-12 00:14:01 +0100

    The machine view now uses a template view

--- make_queue/views/reservation/machine.py
+++ make_queue/views/reservation/machine.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 from make_queue.models import Machine
 
 
-class MachineView(View):
+class MachineView(TemplateView):
+    """View that shows all the machines"""
     template_name = "make_queue/reservation_machines.html"
 
-    def get(self, request):
-        render_parameters = {'machine_types': [
-            {
-                'name': machine_type.literal,
-                'machines': [machine for machine in machine_type.objects.all()]
-            } for machine_type in Machine.__subclasses__() if machine_type.objects.exists()
-        ]}
+    def get_context_data(self):
+        """
+        Creates the context required for the template
 
-        return render(request, self.template_name, render_parameters)
+        :return: A list of all machine types with a list of their machine if there exists at least one machine of that
+                type
+        """
+        return {"machine_types": [{
+            "name": machine_type.literal, "machines": [list(machine_type.objects.all())]
+        } for machine_type in Machine.__subclasses__() if machine_type.objects.exists()]}
