8433b5d0a9583250bfba5c591cc64e9cdb84952d
Shahzeb <33691958+shahzebs@users.noreply.github.com>
2018-05-04 20:12:30 +0200

    Fix profile delete

--- checkin/views.py
+++ checkin/views.py
@@ -202,7 +202,7 @@ class RegisterProfileView(TemplateView):
             data['scan_is_recent'] = scan_is_recent
             if scan_is_recent:
                 Profile.objects.filter(user=request.user).update(card_id=RegisterProfile.objects.first().card_id)
-        Profile.objects.all().delete()
+        RegisterProfile.objects.all().delete()
         return JsonResponse(data)
 
 
