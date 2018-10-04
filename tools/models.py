242c65cf080020c919daabffcdb09037e23383d6
HaraldWilhelmsen <wil.harald@gmail.com>
2018-10-04 19:01:28 +0200

    Created a new Tool model

--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,10 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
+class Tool(models.Model):
+    title = models.CharField(max_length=100, verbose_name=('Title'),)
+    image = models.ImageField(verbose_name=('Image'), blank=True,)
+    content = RichTextField()
+
 # Create your models here.
+
