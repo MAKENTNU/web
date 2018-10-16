eb0e3c72ebe95195b4f90635a78b257e3f72d0a1
HaraldWilhelmsen <wil.harald@gmail.com>
2018-10-16 19:04:11 +0200

    Working progress

--- tools/urls.py
+++ tools/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.contrib.auth.decorators import login_required
+from django.urls import path, register_converter
 
-from tools.views import ToolView
+from tools.views import ToolView, ViewToolsView, ViewToolView
 
 urlpatterns = [
-    url(r'^$', ToolView.as_view()),
+    path(r'', ViewToolsView.as_view(), name='tools'),
+    path(r'<int:pk>/', ViewToolView.as_view(), name='tools'),
 ]
--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,30 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+from tools.models import Tool
 
+
+
+class ViewToolView(TemplateView):
+    template_name = 'tools/tool.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tool = get_object_or_404(Tool, pk=kwargs['pk'])
+        context.update({
+            'tool': tool,
+        })
+        return context
+
+
+class ViewToolsView(TemplateView):
+    template_name = 'tools/tools.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'tools_list': Tool.objects.all()
+        })
+        return context
 
 class ToolView(TemplateView):
-    template_name = 'tools/article.html'
-
+    template_name = 'tools/articles.html'
