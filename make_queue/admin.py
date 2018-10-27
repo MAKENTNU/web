128236a00c22c9f355f935042a53b076ba41d159
zootos <halvard.hummel@hotmail.com>
2018-10-27 11:10:12 +0200

    Override machine type semantic ui widget in admin panel

--- make_queue/admin.py
+++ make_queue/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
+from django.forms import Select
 
 from dataporten.login_handlers import register_handler
-from make_queue.models.models import *
 from make_queue.models.course import *
-
+from make_queue.models.models import *
 # Register your models here.
 from make_queue.util.login_handlers import PrinterHandler
 
-admin.site.register(ReservationRule)
+
+class MachineTypeOverride(admin.ModelAdmin):
+    """
+    Class used to override the semantic ui select widget of the machine type field
+    """
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        if db_field.name == "machine_type":
+            kwargs["widget"] = Select()
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
+
+
+admin.site.register(ReservationRule, MachineTypeOverride)
 admin.site.register(Reservation)
-admin.site.register(Machine)
-admin.site.register(Quota)
+admin.site.register(Machine, MachineTypeOverride)
+admin.site.register(Quota, MachineTypeOverride)
 admin.site.register(Printer3DCourse)
 
 register_handler(PrinterHandler, "printer_allowed")
