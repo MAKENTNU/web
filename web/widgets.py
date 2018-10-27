7cd1541c23d02ebdb93b7efeef49f116f5b58124
zootos <halvard.hummel@hotmail.com>
2018-10-27 10:20:23 +0200

    Select widget using Semantic searchable dropdown selection

--- web/widgets.py
+++ web/widgets.py
@@ -1,4 +1,5 @@
 import django.forms as forms
+from django.utils.translation import gettext_lazy as _
 
 
 class SemanticTimeInput(forms.TimeInput):
@@ -7,3 +8,15 @@ class SemanticTimeInput(forms.TimeInput):
 
 class SemanticChoiceInput(forms.Select):
     template_name = "web/forms/widgets/semantic_select.html"
+
+
+class SemanticSearchableChoiceInput(forms.Select):
+    template_name = "web/forms/widgets/semantic_search_select.html"
+    prompt_text = _("Choose value")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        if "prompt_text" in kwargs:
+            self.attrs["prompt_text"] = kwargs["prompt_text"]
+        else:
+            self.attrs["prompt_text"] = self.prompt_text
