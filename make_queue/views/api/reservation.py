bbd41b21838075ffeb32502af663f7a9da06cf20
zootos <halvard.hummel@hotmail.com>
2018-10-28 21:41:07 +0100

    Cleanup

--- make_queue/views/api/reservation.py
+++ make_queue/views/api/reservation.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -25,22 +23,3 @@ def get_machine_data(request, machine, reservation=None):
                 "max_hours_crossed": rule.max_inside_border_crossed,
             } for rule in ReservationRule.objects.filter(machine_type=machine.machine_type)]
     })
-
-
-def get_reservations_day_and_machine(request, machine, date):
-    reservations = machine.get_reservation_set().filter(start_time__lt=date + timedelta(days=1)).filter(
-        end_time__gte=date)
-
-    return JsonResponse({"reservations": reservations, "date": date})
-
-
-def get_future_reservations_machine_without_specific_reservation(request, reservation):
-    reservations = reservation.machine.get_reservation_set().filter(end_time__gte=timezone.now()).exclude(
-        pk=reservation.pk)
-
-    return JsonResponse(format_reservations_for_start_end_json(reservations))
-
-
-def format_reservations_for_start_end_json(reservations):
-    return {"reservations": [{"start_date": reservation.start_time, "end_date": reservation.end_time} for reservation in
-                             reservations]}
