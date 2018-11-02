28a7d501432d30095475a60cbe741bcbf5dd00a5
zootos <halvard.hummel@hotmail.com>
2018-11-02 11:49:07 +0100

    MachineTypeField test

--- make_queue/tests/models/test_machines.py
+++ make_queue/tests/models/test_machines.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
+from django.core.exceptions import ValidationError
 
 from django.test import TestCase
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 
-from make_queue.fields import MachineTypeField
+from make_queue.fields import MachineTypeField, can_use_3d_printer
 from make_queue.models.course import Printer3DCourse
 from make_queue.models.models import Machine, Quota, Reservation
 
@@ -45,3 +46,65 @@ class TestGenericMachine(TestCase):
         printer.status = "M"
         self.assertEquals(printer.get_status(), "M")
         self.assertEquals(printer.get_status_display(), "Vedlikehold")
+
+
+class TestCanUse3DPrinter(TestCase):
+
+    def setUp(self):
+        self.machine_type = MachineTypeField.get_machine_type(1)
+
+    def test_can_user_3d_printer_not_authenticated(self):
+        self.assertFalse(can_use_3d_printer(AnonymousUser()))
+
+    def test_registered_user_use_3d_printer(self):
+        user = User.objects.create_user("test")
+        Printer3DCourse.objects.create(user=user, username=user.username, name=user.get_full_name(),
+                                       date=timezone.now().date())
+        self.assertTrue(can_use_3d_printer(user))
+
+    def test_registered_username_for_user_use_3d_printer(self):
+        user = User.objects.create_user("test")
+        Printer3DCourse.objects.create(username=user.username, name=user.get_full_name(), date=timezone.now().date())
+        self.assertTrue(can_use_3d_printer(user))
+
+    def test_unregistered_user_use_3d_printer(self):
+        user = User.objects.create_user("test")
+        self.assertFalse(can_use_3d_printer(user))
+
+
+class TestGetMachineType(TestCase):
+
+    def test_get_machine_type(self):
+        for machine_type in MachineTypeField.possible_machine_types:
+            self.assertEqual(machine_type, MachineTypeField.get_machine_type(machine_type.id))
+
+        self.assertEqual(None, MachineTypeField.get_machine_type(0))
+
+
+class TestMachineTypeField(TestCase):
+
+    def test_to_python(self):
+        machine_type_field = MachineTypeField()
+        self.assertEqual(None, machine_type_field.to_python(None))
+        for machine_type in MachineTypeField.possible_machine_types:
+            self.assertEqual(machine_type, machine_type_field.to_python(machine_type.id))
+            self.assertEqual(machine_type, machine_type_field.to_python(machine_type.id))
+
+        try:
+            machine_type_field.to_python(0)
+            self.fail("Invalid IDs should fail when converting to python")
+        except ValidationError:
+            pass
+
+        try:
+            machine_type_field.to_python(machine_type_field)
+            self.fail("Invalid types should fail when converting to python")
+        except ValidationError:
+            pass
+
+    def test_get_prep_value(self):
+        machine_type_field = MachineTypeField()
+        self.assertEqual(None, machine_type_field.get_prep_value(None))
+        for machine_type in MachineTypeField.possible_machine_types:
+            self.assertEqual(machine_type.id, machine_type_field.get_prep_value(machine_type))
+            self.assertEqual(machine_type.id, machine_type_field.get_prep_value([machine_type]))
