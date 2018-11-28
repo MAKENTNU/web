ec091167d6f59e3162c6ef5193af9c7496db5712
Herman Berget <32880010+hermabe@users.noreply.github.com>
2018-11-28 22:21:47 +0100

    Now looks in database if user exists before ldap search

--- make_queue/views/api/course.py
+++ make_queue/views/api/course.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponseNotFound
 import ldap
 
 LDAP_HOST = 'ldap://at.ntnu.no'
@@ -14,20 +16,25 @@ fields = {
 
 
 def LDAP_search(field, value):
-    l = ldap.initialize(LDAP_HOST)
+    l = ldap.initialize(LDAP_HOST, bytes_mode=False)
     l.simple_bind()
-    query = "({}={})".format(field, value)
+    query = "({}={})".format(fields[field], value)
     return l.search_s(LDAP_BASE, ldap.SCOPE_SUBTREE, query)
 
 
 def get_LDAP_field(ldapData, field):
-    if len(ldapData) == 0: # No results
-        return ""
-    return ldapData[0][1][field][0].decode()
+    return ldapData[0][1][fields[field]][0].decode()
+    # list<tuple<str, dict<str, list<str>>>>
 
 
 def get_user_details_from_username(request, username):
-    return JsonResponse({
-        "full_name": get_LDAP_field(LDAP_search(fields['username'], username), fields['full_name']),
-        "username": username,
-    })
+    user = User.objects.filter(username=username).first()
+    user_details = {"username": username}
+    if user:
+        user_details["full_name"] = user.first_name + ' ' + user.last_name
+    else:
+        ldap_results = LDAP_search("username", username)
+        if len(ldap_results) == 0:
+            return HttpResponseNotFound()
+        user_details["full_name"] = get_LDAP_field(ldap_results, "full_name")
+    return JsonResponse(user_details)
