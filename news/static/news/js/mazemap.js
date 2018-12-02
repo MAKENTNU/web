e4473e7b82ebbdf10465d0f570cea253677e7d76
Herman Berget <32880010+hermabe@users.noreply.github.com>
2018-12-02 18:25:49 +0100

    Misc. small fixes and improvements

--- news/static/news/js/mazemap.js
+++ news/static/news/js/mazemap.js
@@ -9,18 +9,20 @@ $('.ui.search')
                             item.dispPoiNames[0] +
                             (item.dispBldNames[0] ? ", " + item.dispBldNames[0] : "")
                         ).replace(/<[/]?em>/g, ""),
-                        description: item.dispPoiNames[1] ? item.dispPoiNames[1] : "",
+                        description: (item.dispPoiNames[1] ? item.dispPoiNames[1] : "").replace(/<[/]?em>/g, ""),
                         id: item.poiId,
                     });
                 });
                 return response;
             },
-            url: 'https://api.mazemap.com/search/equery/?q={query}&rows=5&start=0&withpois=true&withbuilding=false&withtype=false&withcampus=false&campusid=1',
+            url: 'https://api.mazemap.com/search/equery/?q={query}&rows=5&withpois=true&withbuilding=false&withtype=false&withcampus=false&campusid=1',
         },
         onSelect: function (result, response) {
             $("input[name=place_url").val(
                 `https://use.mazemap.com/?campusid=1&desttype=poi&dest=${result.id}`
             );
         },
+        searchDelay: 0,
+        fullTextSearch: true,
     })
 ;
