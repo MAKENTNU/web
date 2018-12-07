1292a733dc14bdfeaac762bc5ea8891f751c9882
zootos <halvard.hummel@hotmail.com>
2018-12-07 17:22:48 +0100

    Moved login logic back to dataporten view. Login now queries ldap for real NTNU username

--- dataporten/views.py
+++ dataporten/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.conf import settings
 from social_django.views import complete
-from .login_handlers import run_handlers
+
+from dataporten.ldap_utils import get_user_details_from_email
 
 
 class Logout(View):
@@ -14,5 +15,20 @@ class Logout(View):
 
 def login_wrapper(request, backend, *args, **kwargs):
     response = complete(request, backend, *args, **kwargs)
-    run_handlers(request.user)
+    user = request.user
+    data = user.social_auth.first().extra_data
+
+    # Update the full name of the user
+    user.first_name = ' '.join(data['fullname'].split()[:-1])
+    user.last_name = data['fullname'].split()[-1]
+
+    # Try to retrieve username from NTNUs LDAP server. Otherwise use the first part of the email as the username
+    ldap_data = get_user_details_from_email(user.email, use_cached=False)
+    if ldap_data:
+        user.username = ldap_data["username"]
+    else:
+        user.username = user.email.split('@')[0]
+
+    user.save()
+
     return response
