4a482730b3e89f22618e854e0f5a8606bcdca5a7
Herman Berget <32880010+hermabe@users.noreply.github.com>
2018-12-08 19:40:58 +0100

    Cleanup and translation

--- news/static/news/js/mazemap.js
+++ news/static/news/js/mazemap.js
@@ -1,28 +1,24 @@
-$('.ui.search')
-    .search({
-        apiSettings: {
-            onResponse: function (mazemap_response) {
-                let response = {results: []};
-                $.each(mazemap_response.result, function (index, item) {
-                    response.results.push({
-                        title: (
-                            item.dispPoiNames[0] +
-                            (item.dispBldNames[0] ? ", " + item.dispBldNames[0] : "")
-                        ).replace(/<[/]?em>/g, ""),
-                        description: (item.dispPoiNames[1] ? item.dispPoiNames[1] : "").replace(/<[/]?em>/g, ""),
-                        id: item.poiId,
-                    });
-                });
-                return response;
-            },
-            url: 'https://api.mazemap.com/search/equery/?q={query}&rows=5&withpois=true&withbuilding=false&withtype=false&withcampus=false&campusid=1',
-        },
-        onSelect: function (result, response) {
-            $("input[name=place_url").val(
-                `https://use.mazemap.com/?campusid=1&desttype=poi&dest=${result.id}`
-            );
-        },
-        searchDelay: 0,
-        fullTextSearch: true,
-    })
-;
+$('.ui.search').search({
+    apiSettings: {
+        // Search for points of interest at NTNU Gløshaugen
+        url: 'https://api.mazemap.com/search/equery/?q={query}&rows=5&withpois=true&campusid=1',
+        // Convert mazemap search results to fit semantic ui search format
+        onResponse: (mazemap_response => ({
+            results: mazemap_response.result.map(item => ({
+                title: (
+                    item.dispPoiNames[0] +
+                    (item.dispBldNames[0] ? ", " + item.dispBldNames[0] : "")
+                ).replace(/<[/]?em>/g, ""),
+                description: (item.dispPoiNames[1] ? item.dispPoiNames[1] : "").replace(/<[/]?em>/g, ""),
+                id: item.poiId,
+            }))
+        })),
+    },
+    onSelect: function (result, response) {
+        $("input[name=place_url").val(
+            `https://use.mazemap.com/?campusid=1&desttype=poi&dest=${result.id}`
+        );
+    },
+    searchDelay: 0,
+    fullTextSearch: true,
+});
