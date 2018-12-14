1a25131a52acee223c5786adef5228ac3fa61761
Herman Berget <32880010+hermabe@users.noreply.github.com>
2018-12-14 22:03:13 +0100

    Moved javascript to separate file and simplified widget class

--- web/widgets.py
+++ web/widgets.py
@@ -1,5 +1,6 @@
 import django.forms as forms
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
 
 
 class SemanticTimeInput(forms.TimeInput):
@@ -32,6 +33,9 @@ class MazemapSearchInput(forms.TextInput):
     """
     template_name = "web/forms/widgets/mazemap_search.html"
 
+    class Media:
+        js = ('web/js/mazemap-search.js', reverse_lazy("javascript-catalog"))
+
     def __init__(self, campus_id=1, max_results=5, url_field=None, attrs=None):
         """
         :param campus_id: Campus to search for points of interest on. Default: NTNU Gløshaugen
@@ -39,11 +43,12 @@ class MazemapSearchInput(forms.TextInput):
         :param url_field: Field to autofill with mazemap url. If None, autofill functionality is turned off
         :param attrs: HTML attributes for the <input> element
         """
-        self.campus_id = campus_id
-        self.max_results = max_results
-        self.url_field = url_field
+
         default_attrs = {
             "placeholder": _("Search places"),
+            "data-campusId": campus_id,
+            "data-maxResults": max_results,
+            "data-urlField": url_field,
         }
         required_class_attr = "prompt"
 
@@ -53,12 +58,3 @@ class MazemapSearchInput(forms.TextInput):
         default_attrs["class"] = default_attrs.get("class", "") + " " + required_class_attr
 
         super().__init__(attrs=default_attrs)
-
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context["widget"].update({
-            "campus_id": self.campus_id,
-            "max_results": self.max_results,
-            "url_field": self.url_field,
-        })
-        return context
