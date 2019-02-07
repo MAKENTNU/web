7126274deb0ee9cb3ce931869106e9cae890f11a
HaraldWilhelmsen <wil.harald@gmail.com>
2019-02-07 17:03:13 +0100

    added a test and changed admin file to register Tools

--- tools/admin.py
+++ tools/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 from tools.models import Tool
+from web.multilingual.database import MultiLingualFieldAdmin
 
-admin.site.register(Tool)
+admin.site.register(Tool, MultiLingualFieldAdmin)
--- tools/tests.py
+++ tools/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from .models import Tool
+
+
 # Create your tests here.
+class Test_a_model(TestCase):
+    def setUp(self):
+        self.first_tool = Tool()
+        # Dette blir lagret til en hver tid:
+
+    def test_create_a_tool(self):
+        self.first_tool.title = 'test tool'
+        self.assertEqual(self.first_tool.title,'test tool')
