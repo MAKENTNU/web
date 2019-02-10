f5c4cc286a3f0c9b3e2016dbed605324156f91c4
zootos <halvard.hummel@hotmail.com>
2019-02-10 11:21:11 +0100

    Fix an issue where empty non required fields would result in an IndexError being thrown

--- web/multilingual/form.py
+++ web/multilingual/form.py
@@ -22,7 +22,11 @@ class MultiLingualFormField(forms.MultiValueField):
         """
         structure = MultiLingualTextStructure("", True)
         for index, language in enumerate(structure.supported_languages):
-            structure[language] = data_list[index]
+            # Non required fields may not have enough values
+            try:
+                structure[language] = data_list[index]
+            except IndexError:
+                pass
         return structure
 
     def __init__(self, *args, **kwargs):
