4ad6d6f4ff3acc358c2e131a47556fd0440f88a0
HaraldWilhelmsen <wil.harald@gmail.com>
2019-02-14 17:41:55 +0100

    add a view to the Tools admin page, with an empty admin page

--- tools/urls.py
+++ tools/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, register_converter
 
-from tools.views import ToolView, ViewToolsView, ViewToolView
+from tools.views import ViewToolsView, ViewToolView, ViewAdminWiev
 
 urlpatterns = [
     path(r'', ViewToolsView.as_view(), name='tools'),
     path(r'<int:pk>/', ViewToolView.as_view(), name='tools'),
-    path(r'admin', ViewToolsView.as_view(), name='tools/admin'), # TODO: fikse viewet!
+    path(r'admin', ViewAdminWiev.as_view(), name='tools/admin'),
 ]
--- tools/views.py
+++ tools/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 from tools.models import Tool
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-
-class ViewToolView(TemplateView):
+class ViewToolView(DetailView):
+    model = Tool
     template_name = 'tools/tool.html'
+    context_object_name = 'tool'
 
+    """
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tool = get_object_or_404(Tool, pk=kwargs['pk'])
@@ -13,17 +16,35 @@ class ViewToolView(TemplateView):
             'tool': tool,
         })
         return context
+    """
 
 
-class ViewToolsView(TemplateView):
+class ViewToolsView(ListView):
+    model = Tool
     template_name = 'tools/tools.html'
+    context_object_name = 'tools_list'
 
+    """
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
             'tools_list': Tool.objects.all()
         })
         return context
+    """
 
-class ToolView(TemplateView):
-    template_name = 'tools/articles.html'
+
+class ViewAdminWiev(PermissionRequiredMixin, ListView):
+    model = Tool
+    template_name = 'tools/admin_tool.html'
+    context_object_name = 'tools_list'
+    permission_required = 'tools.add_Tool'
+
+    """
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'tools_list': Tool.objects.all()
+        })
+        return context
+    """
