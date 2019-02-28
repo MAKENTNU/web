c382228111337e55e55936a38b24af55d610dd11
HaraldWilhelmsen <wil.harald@gmail.com>
2019-02-28 17:33:27 +0100

    add admin panel to create and edit a tool

--- tools/views.py
+++ tools/views.py
@@ -1,37 +1,20 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 from tools.models import Tool
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+
 
 class ViewToolView(DetailView):
     model = Tool
     template_name = 'tools/tool.html'
     context_object_name = 'tool'
 
-    """
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        tool = get_object_or_404(Tool, pk=kwargs['pk'])
-        context.update({
-            'tool': tool,
-        })
-        return context
-    """
-
 
 class ViewToolsView(ListView):
     model = Tool
     template_name = 'tools/tools.html'
     context_object_name = 'tools_list'
-
-    """
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'tools_list': Tool.objects.all()
-        })
-        return context
-    """
 
 
 class ViewAdminWiev(PermissionRequiredMixin, ListView):
@@ -40,11 +23,29 @@ class ViewAdminWiev(PermissionRequiredMixin, ListView):
     context_object_name = 'tools_list'
     permission_required = 'tools.add_Tool'
 
-    """
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'tools_list': Tool.objects.all()
-        })
-        return context
-    """
+
+class ViewAdminCreateVeiw(PermissionRequiredMixin, CreateView):
+    model = Tool
+    template_name = 'tools/admin_tool_create.html'
+    context_object_name = 'tool'
+    permission_required = 'tools.add_Tool'
+    fields = (
+        'title',
+        'content',
+        'image',
+    )
+    success_url = reverse_lazy('tools/admin')
+
+
+class ViewAdminEditVeiw(PermissionRequiredMixin, UpdateView):
+    model = Tool
+
+    template_name = 'tools/admin_tool_edit.html'
+    context_object_name = 'tool'
+    permission_required = 'tools.add_Tool'
+    fields = (
+        'title',
+        'content',
+        'image',
+    )
+    success_url = reverse_lazy('tools/admin')
