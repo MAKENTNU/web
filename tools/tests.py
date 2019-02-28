3574f7b6089458a4c39ae35cdc2e6da9178c1851
HaraldWilhelmsen <wil.harald@gmail.com>
2019-02-28 18:01:58 +0100

    add delete option

--- tools/tests.py
+++ tools/tests.py
@@ -4,11 +4,12 @@ from .models import Tool
 
 
 # Create your tests here.
-class Test_a_model(TestCase):
+class ToolTestCase(TestCase):
     def setUp(self):
         self.first_tool = Tool()
-        # Dette blir lagret til en hver tid:
 
     def test_create_a_tool(self):
         self.first_tool.title = 'test tool'
         self.assertEqual(self.first_tool.title,'test tool')
+
+
--- tools/urls.py
+++ tools/urls.py
@@ -5,8 +5,8 @@ from tools import views
 urlpatterns = [
     path(r'', views.ViewToolsView.as_view(), name='tools'),
     path(r'<int:pk>/', views.ViewToolView.as_view(), name='tools'),
-    path(r'admin', views.ViewAdminWiev.as_view(), name='tools/admin'),
-    path(r'admin/create', views.ViewAdminCreateVeiw.as_view(), name='tools/create'),
-    path(r'admin/<int:pk>/edit', views.ViewAdminEditVeiw.as_view(), name='tools/edit'),
-
+    path(r'admin', views.ViewAdminView.as_view(), name='tools/admin'),
+    path(r'admin/create', views.ViewAdminCreateView.as_view(), name='tools/create'),
+    path(r'admin/<int:pk>/edit', views.ViewAdminEditView.as_view(), name='tools/edit'),
+    path(r'admin/<int:pk>/delete', views.ViewDeleteView.as_view(), name='tools/delete'),
 ]
