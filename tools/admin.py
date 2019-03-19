eb90f236b4daca870aad02ff1eb12c8ab318a75c
HaraldWilhelmsen <wil.harald@gmail.com>
2019-03-19 19:23:41 +0100

    merge

--- tools/admin.py
+++ tools/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
 from tools.models import Tool
-<<<<<<< HEAD
+
 from web.multilingual.database import MultiLingualFieldAdmin
 
 admin.site.register(Tool, MultiLingualFieldAdmin)
-=======
-
-admin.site.register(Tool)
->>>>>>> 1a41b3b99af06cb8f8814169aa7ab0626fa2439e
