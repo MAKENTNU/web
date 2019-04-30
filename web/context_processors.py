c9238a91ec249ca1260762dfafd1218ada3781f7
Dabble <ddabble@users.noreply.github.com>
2019-04-30 13:28:19 +0200

    Added context processor for login button
    This moves the logic from the template to the context processor.
    
    Also, now keeps the same `next` parameter value if the login button is pressed multiple times, and fixed being redirected to the login page after logging in if one has pressed the login button a second time.

---
+++ web/context_processors.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import reverse
+
+
+def login(request):
+    # The current `next` parameter
+    current_redirect_url = LoginView(request=request).get_redirect_url()
+
+    # If at the login page:
+    if request.path.startswith(reverse('login').rstrip("/")):
+        # Let the `next` parameter be the same as it currently is
+        login_redirect_path = current_redirect_url
+    # If at the front page:
+    elif request.path == "/":
+        # Don't redirect after login
+        login_redirect_path = None
+    else:
+        # Redirect back to the same page after logging in
+        login_redirect_path = request.path
+
+    return {
+        'login_next_param': ("?next=" + login_redirect_path) if login_redirect_path else "",
+    }
