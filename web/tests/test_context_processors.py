032d2705a6d6426b66e8fa919d5a79712f81c5b4
Dabble <ddabble@users.noreply.github.com>
2019-07-24 20:14:28 +0200

    Added tests for login context processor

---
+++ web/tests/test_context_processors.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django_hosts import reverse
+
+
+class TestLoginRedirect(TestCase):
+    def test_front_page_has_no_next_parameter(self):
+        response = self.client.get("/")
+        self.assertEqual(response.context['login_next_param'], "")
+
+    def test_login_page_keeps_redirect_target(self):
+        login_path = reverse('login', host="main")
+        response = self.client.get(login_path + "?next=/asdf/")
+        login_redirect_path = self.get_login_redirect_path(response)
+        self.assertEqual(login_redirect_path, "/asdf/")
+
+    def test_other_page_redirects_back(self):
+        # Should work regardless of the path, as non-existant paths return a 404 page with the same context variables
+        response = self.client.get("/about/")
+        login_redirect_path = self.get_login_redirect_path(response)
+        self.assertEqual(login_redirect_path, "/about/")
+
+    @staticmethod
+    def get_login_redirect_path(response):
+        return response.context['login_next_param'].split("?next=")[-1]
