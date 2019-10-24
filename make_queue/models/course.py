dbfc0a5db3301d42e31fd3ece37a51287d17daec
Herman Berget <32880010+hermabe@users.noreply.github.com>
2019-10-24 19:30:13 +0200

    save() mathod for Printer3DCourse syncing card number and username with user

--- make_queue/models/course.py
+++ make_queue/models/course.py
@@ -46,3 +46,32 @@ class Printer3DCourse(models.Model):
             self._card_number = None
         else:
             self._card_number = card_number
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.pk is None:  # Creation of new object
+            self._connect_to_user()
+        else:
+            old = Printer3DCourse.objects.get(pk=self.pk)
+            if old.username != self.username:
+                # Changed username, connect to new user
+                self._connect_to_user()
+            elif self.user:
+                # Update username in the rare case that a user changes their username
+                self.username = self.user.username
+
+        # If user is set, use card number from user
+        if self.user and self._card_number:
+            self.user.card_number = self._card_number
+            self._card_number = None
+            self.user.save()
+
+        super().save(force_insert, force_update, using, update_fields)
+
+    def _connect_to_user(self):
+        """
+        Connect to user with username if user exists
+        """
+        try:
+            self.user = User.objects.get(username=self.username)
+        except User.DoesNotExist:
+            pass
