b2020659191f8113c3f20e30dc44a72715543fc1
sigtheidiot <sigridge@stud.ntnu.no>
2019-11-13 17:19:29 +0100

    remove filter for matinence

--- make_queue/views/reservation/reservation.py
+++ make_queue/views/reservation/reservation.py
@@ -298,6 +298,7 @@ class FindFreeSlot(FormView):
 
         periods = []
         for machine in Machine.objects.filter(machine_type=form.cleaned_data["machine_type"]):
+            # Checks if the machine is not out-of-order
             if not machine.get_status() in "O":
                 periods += self.get_periods(machine, required_time)
 
