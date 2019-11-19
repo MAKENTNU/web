c8bd7aa441e60da8ceb0587eaed8b86ba9601bcc
Dabble <ddabble@users.noreply.github.com>
2019-11-19 22:44:47 +0100

    Improved comments

--- web/static/web/js/widgets/mazemap-search.js
+++ web/static/web/js/widgets/mazemap-search.js
@@ -11,7 +11,7 @@ jQuery.fn.extend({
                     `&rows=${data.maxresults}` +
                     `&campusid=${data.campusid}`,
 
-                // Convert MazeMap search results to fit Semantic UI search format
+                // Convert MazeMap search results to fit Fomantic UI search format
                 onResponse: (mazemapResponse => ({
                     results: mazemapResponse.result.map(item => ({
                         title: (
@@ -24,7 +24,7 @@ jQuery.fn.extend({
                 })),
             },
             templates: {
-                // Template for error messages. Default provided by Semantic UI but with translations
+                // Template for error messages. Default provided by Fomantic UI but with translations
                 message: (type, message) => (
                     `<div class="message empty">
                     <div class="header">
