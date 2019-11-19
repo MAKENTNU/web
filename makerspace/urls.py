0e79aac637af930eb18dbedb2fddbf05a27d2704
qpb96 <quitpb@stud.ntnu.no>
2019-11-19 20:49:54 +0100

    Modify admin_tool.html. Add translation, add button, make delete icon more obvious

--- makerspace/urls.py
+++ makerspace/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('', ViewMakerspaceView.as_view(), name='makerspace'),
     path(r'admin/<int:pk>/edit/', login_required(EditMakerspaceView.as_view()), name='makerspace-edit'),
     path(r'tools/', ToolsView.as_view(), name='makerspace-tools'),
-    path(r'tool/<int:pk>/', ToolView.as_view(), name='makerspace-tool'),
     path(r'tools/admin', login_required(AdminToolView.as_view()), name='makerspace-tools-admin'),
     path(r'tools/admin/create', login_required(CreateToolView.as_view()), name='makerspace-tools-create'),
     path(r'tools/admin/<int:pk>/edit', login_required(EditToolView.as_view()), name='makerspace-tools-edit'),
     path(r'tools/admin/<int:pk>/delete', login_required(DeleteToolView.as_view()), name='makerspace-tools-delete'),
+    path(r'tool/<int:pk>/', ToolView.as_view(), name='makerspace-tool'),
+
 ]
