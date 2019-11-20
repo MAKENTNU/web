8822911534ab393398f95cef45c6dfc328ea3d93
shahzebs <shahzebs@stud.ntnu.no>
2019-11-20 18:27:37 +0100

    Add search feature to inventory list using contains and ajax

--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
-# Create your views here.
-from django.views.generic import TemplateView
 from django.db.models import Q
+from django.http import JsonResponse
+from django.views.generic import TemplateView
 
-from inventory.models import ItemInSubContainer, Item, Category
+from inventory.models import Item, Category
 
 
 class InventoryView(TemplateView):
@@ -35,9 +33,7 @@ class ItemView(TemplateView):
 
 
 class SearchItemsView(TemplateView):
-    template_name = "inventory/inventory_search.html"
-
-    MAX_RESULTS = 10
+    template_name = "inventory/inventory.html"
 
     def post(self, request):
         search_string = request.POST.get('search')
@@ -45,10 +41,28 @@ class SearchItemsView(TemplateView):
         search_string = search_string.lower().strip()
 
         item_filter = Q(name__contains=search_string) | Q(description__contains=search_string)
-        matched_items = Item.objects.filter(item_filter).order_by('name')[:self.MAX_RESULTS]
+        matched_items = Item.objects.filter(item_filter).order_by('name').prefetch_related('categories',
+                                                                                           'subcontainers__container',
+                                                                                           'items_in_subcontainer')
+
+        # Maybe this can be optimized by serializing the QuerySet matched_items?
+        result = []
+        for item in matched_items:
+            item_dict = {}
+            item_dict['pk'] = item.pk
+            item_dict['name'] = item.name
+            item_dict['categories'] = item.category_html_list
+            item_dict['amount'] = item.total_amount
+            if item.unit:
+                item_dict['unit'] = item.unit
+            else:
+                item_dict['unit'] = ''
+            item_dict['container'] = item.container_html_list
+            item_dict['comment'] = item.comment
+            result.append(item_dict)
 
         data = {
-            "item_names": [item.name for item in matched_items],
+            "matched_items": result,
         }
 
         return JsonResponse(data)
