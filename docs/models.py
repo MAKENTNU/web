e156bef81393dfa0011cfb01005e071a6620757d
zootos <halvard.hummel@hotmail.com>
2019-12-15 16:03:17 +0100

    Associate edits and pages to users

--- docs/models.py
+++ docs/models.py
@@ -1,4 +1,5 @@
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -10,6 +11,7 @@ class Page(models.Model):
     title_validator = RegexValidator(regex=title_regex,
                                      message=_("Only numbers, letters, space, parenthesises and colon are allowed"))
     title = models.CharField(max_length=64, unique=True, verbose_name=_("Title"), validators=[title_validator])
+    created_by = models.ForeignKey(User, models.DO_NOTHING, null=True, blank=True)
 
     @property
     def content(self):
@@ -18,9 +20,13 @@ class Page(models.Model):
             return self.content_history.order_by("changed").last()
         return ""
 
+    def __str__(self):
+        return self.title
+
 
 class Content(models.Model):
     """The content of a documentation page. All versions are kept for editing history."""
     page = models.ForeignKey(Page, models.CASCADE, related_name="content_history", verbose_name=_("Page"))
     changed = models.DateTimeField(verbose_name=_("Time changed"))
     content = RichTextUploadingField(verbose_name=_("Content"))
+    made_by = models.ForeignKey(User, models.DO_NOTHING, verbose_name=_("Made by"), null=True, blank=True)
