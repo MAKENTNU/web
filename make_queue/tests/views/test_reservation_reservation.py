f09fb5873e94ecbe9b4129880d45652c53fe1fbc
sigtheidiot <sigridge@stud.ntnu.no>
2020-01-27 09:49:38 +0100

    update test file

--- make_queue/tests/views/test_reservation_reservation.py
+++ make_queue/tests/views/test_reservation_reservation.py
@@ -79,7 +79,7 @@ class ReservationCreateOrChangeViewTest(BaseReservationCreateOrChangeViewTest):
         reservation = Reservation(user=self.user, start_time=form.cleaned_data["start_time"],
                                   end_time=form.cleaned_data["end_time"], machine=self.machine)
         self.assertEqual(view.get_error_message(form, reservation),
-                         "Reservasjoner kan bare lages 7 dager frem i tid")
+                         "Reservations can only be made 7 days ahead of time")
 
     def test_validate_and_save_valid_reservation(self):
         view = self.get_view()
