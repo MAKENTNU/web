a72126d40b25f0b6ab718f576a2f84597606222d
zootos <halvard.hummel@hotmail.com>
2020-02-29 12:14:58 +0100

    Hinder the edit user form from crashing when the given member does not have a card number

--- internal/forms.py
+++ internal/forms.py
@@ -41,7 +41,8 @@ class EditMemberForm(ModelForm):
         super().__init__(**kwargs)
 
         member = kwargs['instance']
-        self.initial['card_number'] = member.user.card_number.number
+        if member.user.card_number:
+            self.initial['card_number'] = member.user.card_number.number
 
         if not user.has_perm("internal.can_edit_group_membership"):
             for field_name in ["committees", "role", "comment", "guidance_exemption", "active", "honorary"]:
