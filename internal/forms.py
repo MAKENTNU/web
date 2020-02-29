2d8ea852ae2af03c16f3e95fc7ed1f56cd050b09
zootos <halvard.hummel@hotmail.com>
2020-02-29 12:14:19 +0100

    Hinder the add user form to crash due to User not being imported

--- internal/forms.py
+++ internal/forms.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 import card.utils
 from card.forms import CardNumberField
 from internal.models import Member, SystemAccess
+from users.models import User
 from web.widgets import SemanticSearchableChoiceInput, SemanticDateInput, SemanticMultipleSelectInput
 
 
