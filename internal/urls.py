0b33f2908e7a774e7d61d5bbb4722232ff08dad7
Dabble <ddabble@users.noreply.github.com>
2020-04-26 04:09:41 +0200

    Replaced decorated_includes() with library
    This fixes a bug that caused anonymous users to get redirected to login when trying to change the site's language. This could be reproduced by:
    1. If not logged in since server restart: Log in and then log out.
    2. Press the button for changing language in the header.
    
    This bug was caused by `decorated_includes()` decorating `django.conf.urls.i18n.set_language()` with `permission_required("internal.is_internal")`. This was done from `internal/urls.py`, but still affected `web/urls.py`.

--- internal/urls.py
+++ internal/urls.py
@@ -1,12 +1,12 @@
+from decorator_include import decorator_include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.auth.decorators import permission_required
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from django_hosts import reverse
 
 from internal.views import MembersListView, AddMemberView, EditMemberView, MemberUndoQuitView, MemberQuitView, \
     MemberUndoRetireView, MemberRetireView, ToggleSystemAccessView
-from web.url_util import decorated_includes
 
 unsafe_urlpatterns = [
     path("members", MembersListView.as_view(), name="members"),
@@ -23,17 +23,17 @@ unsafe_urlpatterns = [
 ]
 
 urlpatterns = [
-    path('robots.txt', TemplateView.as_view(template_name='internal/robots.txt', content_type='text/plain')),
-    path("i18n/", decorated_includes(
+    path("robots.txt", TemplateView.as_view(template_name='internal/robots.txt', content_type='text/plain')),
+    path("i18n/", decorator_include(
         permission_required("internal.is_internal", login_url=reverse("login", host="main")),
-        include("django.conf.urls.i18n")
+        "django.conf.urls.i18n"
     )),
 ]
 
 urlpatterns += i18n_patterns(
-    path("", decorated_includes(
+    path("", decorator_include(
         permission_required("internal.is_internal", login_url=reverse("login", host="main")),
-        include(unsafe_urlpatterns)
+        unsafe_urlpatterns
     )),
     prefix_default_language=False
 )
