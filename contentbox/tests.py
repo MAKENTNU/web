c7a00ee9bb6a658f41a7f7324b43e41a4709b1d4
Dabble <ddabble@users.noreply.github.com>
2020-09-18 13:19:44 +0200

    Refactored contentbox model and views
    `Contentbox`' previous method `get()` was replaced by Django's standard `get_or_create()`, and so the test `test_get()` was also removed.

--- contentbox/tests.py
+++ contentbox/tests.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import Permission
 from users.models import User
 from django.test import TestCase
 
-from contentbox.models import ContentBox, DisplayContentBoxView
+from .models import ContentBox
+from .views import DisplayContentBoxView
 
 
 class ModelTestCase(TestCase):
@@ -12,16 +13,6 @@ class ModelTestCase(TestCase):
         self.assertEqual(contentbox.title, 'TEST_TITLE')
         self.assertEqual(contentbox.title, str(contentbox))
 
-    def test_get(self):
-        self.assertEqual(ContentBox.objects.count(), 0)
-        contentbox = ContentBox.get(title='TEST_TITLE')
-        self.assertEqual(ContentBox.objects.count(), 1)
-        self.assertEqual(contentbox.title, 'TEST_TITLE')
-
-        contentbox2 = ContentBox.get(title='TEST_TITLE')
-        self.assertEqual(ContentBox.objects.count(), 1)
-        self.assertEqual(contentbox, contentbox2)
-
     def test_display_view(self):
         view = DisplayContentBoxView()
         view.title = 'TEST_TITLE'
@@ -30,7 +21,7 @@ class ModelTestCase(TestCase):
         self.assertEqual(context['contentbox'].title, 'TEST_TITLE')
 
     def test_edit_without_permission(self):
-        ContentBox.get(title='TEST_TITLE')
+        ContentBox.objects.get_or_create(title='TEST_TITLE')
         response = self.client.get('/contentbox/1/edit/')
         self.assertNotEqual(response.status_code, 200)
 
@@ -41,6 +32,6 @@ class ModelTestCase(TestCase):
         permission = Permission.objects.get(codename='change_contentbox')
         user.user_permissions.add(permission)
         self.client.login(username=username, password=password)
-        ContentBox.get(title='TEST_TITLE')
+        ContentBox.objects.get_or_create(title='TEST_TITLE')
         response = self.client.get('/contentbox/1/edit/')
         self.assertEqual(response.status_code, 200)
--- contentbox/views.py
+++ contentbox/views.py
@@ -1,8 +1,33 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.urls import reverse
-from django.views.generic import UpdateView
+from django.urls import path, reverse
+from django.views.generic import TemplateView, UpdateView
 
-from contentbox.models import ContentBox
+from .models import ContentBox
+
+
+class DisplayContentBoxView(TemplateView):
+    template_name = 'contentbox/display.html'
+    title = ""
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        contentbox, _created = ContentBox.objects.get_or_create(title=self.title)
+        context.update({
+            'contentbox': contentbox,
+        })
+        return context
+
+    @classmethod
+    def get_path(cls, title: str):
+        return path(f'{title}/', cls.as_view(title=title), name=title)
+
+    @classmethod
+    def get_multi_path(cls, title: str, alt_url1: str, *other_alt_urls: str) -> tuple:
+        alt_urls = (alt_url1, *other_alt_urls)
+        return (
+            path(f'{title}/', cls.as_view(title=title), name=title),
+            *(path(f'{url}/', cls.as_view(title=title)) for url in alt_urls),
+        )
 
 
 class EditContentBoxView(PermissionRequiredMixin, UpdateView):
