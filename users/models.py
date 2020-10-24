1c7804f6073b0327b1a22d8b3122b91077fc3662
Dabble <ddabble@users.noreply.github.com>
2020-10-24 03:41:02 +0200

    Added ldap_full_name field to User model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from card.models import CardNumberField
 
 
 class User(AbstractUser):
+    ldap_full_name = models.CharField(max_length=150, blank=True, verbose_name=_('Full name from LDAP'))
+
     card_number = CardNumberField(unique=True, null=True, blank=True)
