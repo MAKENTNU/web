3ffe19bb6e34b53caeb8fe47c5de65e791863260
Dabble <ddabble@users.noreply.github.com>
2020-10-26 17:13:13 +0100

    Added priority to Equipment
    Also added an EquipmentQuerySet manager to Equipment, with a `default_order_by()` method.

--- makerspace/models.py
+++ makerspace/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.db.models import F
+from django.db.models.functions import Lower
 from django.utils.translation import gettext_lazy as _
 
 from web.multilingual.database import MultiLingualRichTextUploadingField, MultiLingualTextField
+
+
+class EquipmentQuerySet(models.QuerySet):
+
+    def default_order_by(self):
+        return self.order_by(
+            F('priority').asc(nulls_last=True),
+            Lower('title'),
+        )
 
 
 class Equipment(models.Model):
@@ -12,6 +23,14 @@ class Equipment(models.Model):
     )
     image = models.ImageField(upload_to='equipment', verbose_name=_('Image'))
     description = MultiLingualRichTextUploadingField(verbose_name=_('Description'))
+    priority = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name=_("Priority"),
+        help_text=_("If specified, the equipment is sorted ascending by this value."),
+    )
+
+    objects = EquipmentQuerySet.as_manager()
 
     def __str__(self):
         return str(self.title)
