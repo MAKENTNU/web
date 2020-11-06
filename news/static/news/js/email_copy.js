5ca2705bc24791d557b848b3b7b75c828cf5ce3c
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2020-11-06 20:21:36 +0100

    moved script and css to separate file

---
+++ news/static/news/js/email_copy.js
@@ -0,0 +1,32 @@
+var popupTimer;
+
+function delayPopup(popup) {
+    popupTimer = setTimeout(function() { $(popup).popup('hide') }, 4200);
+}
+
+$(document).ready(function () {
+    $('.copyToken').click(function (){
+        console.log("hei")
+        clearTimeout(popupTimer);
+
+        var $input = $(this).closest('div').find('.copyInput');
+
+        /* Select the text field */
+        $input.select();
+
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+
+        $(this)
+            .popup({
+                title    : 'Successfully copied to clipboard!',
+                on: 'manual',
+                exclusive: true
+            })
+            .popup('show')
+        ;
+        // Hide popup after 5 seconds
+        delayPopup(this);
+    });
+
+});
