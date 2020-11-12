e75ff9aa0c8a01f008f7cbf629d37578fae4fbde
mahoyen <32592450+mahoyen@users.noreply.github.com>
2020-11-12 10:51:07 +0100

    added (currently failing) EditMachineFormTests
    They fail due to a `KeyError "machine_type"` in `cleaned_data` in
    `make_queue.forms.EditMachineForm.clean()`.

--- make_queue/tests/forms/test_machine_forms.py
+++ make_queue/tests/forms/test_machine_forms.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 from news.models import Event, TimePlace
-from ...forms import BaseMachineForm
+from ...forms import BaseMachineForm, EditMachineForm
 from ...models.models import Machine, MachineType
 
 class MachineFormTest(TestCase):
@@ -54,6 +54,42 @@ class MachineFormTest(TestCase):
     def test_empty_stream_name_when_not_3dprinter_returns_true(self):
         form_data = self.valid_form_data
         form_data["machine_type"] = MachineType.objects.get(pk=2)
+        form_data["stream_name"] = ""
+
+        form = BaseMachineForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+
+class EditMachineFormTest(TestCase):
+
+    def setUp(self):
+        self.printer_machine_type = MachineType.objects.get(pk=1)
+        self.machine = Machine.objects.create(name="Test", machine_model="Ultimaker 2+", machine_type=self.printer_machine_type)
+        self.valid_form_data = {
+            "name": "Machine_test1",
+            "status": "F",
+            "stream_name": "make-0101",
+            "location": "Here",
+            "location_url": "https://makentnu.no/",
+            "machine_model": "multimaker",
+        }
+
+    def test_valid_machine_form(self):
+        form = EditMachineForm(data=self.valid_form_data)
+        
+        self.assertTrue(form.is_valid())
+
+    def test_empty_stream_name_when_3dprinter_returns_false(self):
+        form_data = self.valid_form_data
+        form_data["stream_name"] = ""
+
+        form = BaseMachineForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_empty_stream_name_when_not_3dprinter_returns_true(self):
+        form_data = self.valid_form_data
+        self.machine.machine_type = MachineType.objects.get(pk=2)
         form_data["stream_name"] = ""
 
         form = BaseMachineForm(data=form_data)
