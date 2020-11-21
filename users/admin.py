77170d9c01d0f8ad1bd322953b0bb64c419cf840
Dabble <ddabble@users.noreply.github.com>
2020-11-21 12:49:12 +0100

    Added custom user fields to UserAdmin
    ...i.e. `ldap_full_name` and `card_number`. The easiest way to do this was by copying some of the fields from Django's `UserAdmin`.
    This also facilitates modifying those fields in the future.

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.template.defaultfilters import urlize
+from django.utils.text import capfirst
+from django.utils.translation import gettext_lazy as _
 
 from util.admin_utils import DefaultAdminWidgetsMixin
 from .models import User
 
 
 class UserAdmin(DefaultAdminWidgetsMixin, DjangoUserAdmin):
-    pass
+    list_display = (
+        'username', 'get_email', 'first_name', 'last_name', 'card_number', 'is_staff', 'is_superuser',
+        'date_joined', 'last_login',
+    )
+    search_fields = ('username', 'first_name', 'last_name', 'ldap_full_name', 'email', 'card_number')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        # `capfirst()` to avoid duplicate translation differing only in case
+        (capfirst(_("personal info")), {'fields': ('first_name', 'last_name', 'ldap_full_name', 'email')}),
+        (capfirst(_("other info")), {'fields': ('card_number',)}),
+        (capfirst(_("permissions")), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (capfirst(_("important dates")), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('ldap_full_name', 'last_login', 'date_joined')
+
+    @admin.display(
+        ordering='email',
+        description=_("Email"),
+    )
+    def get_email(self, user: User):
+        return urlize(user.email) or None
 
 
 admin.site.register(User, UserAdmin)
