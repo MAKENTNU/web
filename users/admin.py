99030a68252dc8fcfee9081006514d0eb71e1189
Dabble <ddabble@users.noreply.github.com>
2020-11-21 12:49:12 +0100

    Added custom user fields to UserAdmin
    The easiest way to do this was by copying some of the fields from Django's `UserAdmin`. This also facilitates modifying those fields in the future.

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
+
+
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'card_number', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'first_name', 'last_name', 'ldap_full_name', 'email', 'card_number')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_("Personal info"), {'fields': ('first_name', 'last_name', 'ldap_full_name', 'email')}),
+        (_("Other info"), {'fields': ('card_number',)}),
+        (_("Permissions"), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_("Important dates"), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('ldap_full_name',)
 
 
 admin.site.register(User, UserAdmin)
