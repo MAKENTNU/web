d875afac95e4543b4fa6b04f12842f2b16c28201
Eirik Ambjornsen <e.ambjornsen@gmail.com>
2021-01-25 15:20:01 +0100

    URL pattern changes

--- faq/urls.py
+++ faq/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FAQPageView.as_view(), name='FAQ'),
-    path('admin', login_required(views.FAQAdminView.as_view()), name="FAQ-admin"),
-    path('admin/create', login_required(views.CreateQuestionView.as_view()), name="FAQ-create"),
-    path('admin/<int:pk>/edit/', login_required(views.EditQuestionView.as_view()), name="FAQ-edit"),
-    path('admin/<int:pk>/delete/', login_required(views.DeleteQuestionView.as_view()), name="FAQ-question-delete"),
+    path('', views.FAQPageView.as_view(), name='faq'),
+    path('admin', login_required(views.FAQAdminView.as_view()), name='faq-admin'),
+    path('admin/create', login_required(views.CreateQuestionView.as_view()), name='faq-create'),
+    path('admin/<int:pk>/edit/', login_required(views.EditQuestionView.as_view()), name='faq-edit'),
+    path('admin/<int:pk>/delete/', login_required(views.DeleteQuestionView.as_view()), name='faq-question-delete'),
 ]
