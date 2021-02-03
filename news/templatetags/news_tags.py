4e71be207dd48e2709354902898e72b07511acc2
Dabble <ddabble@users.noreply.github.com>
2021-02-03 02:50:37 +0100

    Misc. template display improvements
    * [member_list.html] Made it so that only people who can change the committee membership of other members, can see the reason they quit, for better privacy
    * [make_queue/forms.py] Increased `special_text`'s `max_length`, to account for the length of real data
    * [make_queue/forms.py] Replaced `BaseMachineForm`'s `SemanticSearchableChoiceInput` widget for the `status` field with `SemanticChoiceInput`, because there is not really any need to search for a value, as there are always only 3 values to choose from
    * [equipment_list.css] Fixed hiding the top and bottom parts of the equipment title text when on mobile
    * [admin_equipment_edit.html] Added a link to the equipment admin page (ignore whitespace changes for easier viewing)
    * [admin_event_detail.html] Replaced the `i.icons` tag with just an `i.time.icon` tag, as the `i.noshadow.bottom.right.corner.add.icon` was practically invisible
    * [admin_timeplace_listing.html] Replaced the `color` filter with manually inserting the colors, as it makes the code more transparent / easier to read
    * [event_detail.html] Removed the unnecessary `<span>` tag
    * [news_tags.py] Removed the (now) unused `color` filter
    * [style.css] Removed the (now) unused `.noshadow` selector
    * [header.html] Made the "Profile" link only visible to MAKE members, as it has no use for non-members, which gives it potential for creating confusion

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -5,11 +5,6 @@ register = template.Library()
 
 
 @register.filter
-def color(val):
-    return 'yellow' if val else 'grey'
-
-
-@register.filter
 def past(timeplaces):
     return timeplaces.filter(end_time__lte=timezone.now()).order_by("-end_time")
 
