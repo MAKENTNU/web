5b05696accb9b208a97b4dbf31666839188f6ed2
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2021-09-14 21:29:02 +0200

    Made disable stream name field more general

--- make_queue/static/make_queue/js/disable_stream_name_field.js
+++ make_queue/static/make_queue/js/disable_stream_name_field.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
+    const has_stream_dict = JSON.parse(document.getElementById('has-stream-dict').textContent);
     $("select[name=machine_type]").change(function () { 
         var input = document.getElementById("id_stream_name");
-        input.disabled = this.value != 1;
+        input.disabled = !(has_stream_dict[this.value]);
     });
     document.getElementById("id_stream_name").disabled = document.getElementById("id_machine_type").value != 1
 });
