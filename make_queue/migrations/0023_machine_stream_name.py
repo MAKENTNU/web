0498420d323c9426d29622ac8c42449982e5cf4f
Dabble <ddabble@users.noreply.github.com>
2021-10-05 15:43:26 +0200

    Removed redundant reverse_code in Machine migration
    When unapplying the migration, the `stream_name` field is removed entirely, so it's not necessary to set it to `None` first.

--- make_queue/migrations/0023_machine_stream_name.py
+++ make_queue/migrations/0023_machine_stream_name.py
@@ -13,12 +13,6 @@ def set_stream_name(apps, schema_editor):
         machine.save()
 
 
-def revert_stream_name(apps, schema_editor):
-    Machine = apps.get_model('make_queue', 'Machine')
-    db_alias = schema_editor.connection.alias
-    Machine.objects.using(db_alias).filter(machine_type__has_stream=True).update(stream_name=None)
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -31,5 +25,5 @@ class Migration(migrations.Migration):
             name='stream_name',
             field=models.CharField(blank=True, default=None, max_length=30, null=True, validators=[RegexValidator(code='NOT_URL_SAFE', message='Stream name can only consist of english letters, numbers, hyphens or underscores.', regex='^[a-zA-Z0-9_-]+$')], verbose_name='Stream Name'),
         ),
-        migrations.RunPython(set_stream_name, reverse_code=revert_stream_name),
+        migrations.RunPython(set_stream_name),
     ]
