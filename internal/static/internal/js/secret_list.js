bd41e6626a0dfc2042bbedcd1ba700f73d736438
sigtheidiot <sigridge@stud.ntnu.no>
2021-10-05 18:43:34 +0200

    Add button and functionality to hide the secret quickly

--- internal/static/internal/js/secret_list.js
+++ internal/static/internal/js/secret_list.js
@@ -11,3 +11,12 @@ $(".secret-button").click(function () {
         secretButton.removeClass("display-none");
     }, SECRET_SHOW_DURATION_SECONDS * 1000);
 });
+
+$(".secret-close-button").click(function () {
+    const secretCloseButton = $(this);
+    const secretCloseId = secretCloseButton.data("secret-id");
+    const secretButton = $(`#${"button-"+secretCloseId}`);
+    const secret = $(`#${secretCloseId}`);
+    secret.addClass("display-none");
+    secretButton.removeClass("display-none");
+});
