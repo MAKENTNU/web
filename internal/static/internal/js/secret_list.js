9740849a63bbaff17b0e3aee927f6ee783f51381
sigtheidiot <sigridge@stud.ntnu.no>
2021-10-12 16:54:01 +0200

    Remove timer when closing, create function for hiding buttons

--- internal/static/internal/js/secret_list.js
+++ internal/static/internal/js/secret_list.js
@@ -1,22 +1,28 @@
 const SECRET_SHOW_DURATION_SECONDS = 10;
 
+let timer;
+
 $(".secret-button").click(function () {
     const secretButton = $(this);
     const secretId = secretButton.data("secret-id");
     const secret = $(`#${secretId}`);
     secret.removeClass("display-none");
     secretButton.addClass("display-none");
-    setTimeout(() => {
-        secret.addClass("display-none");
-        secretButton.removeClass("display-none");
+    timer = setTimeout(() => {
+        hideButtons(secret, secretButton);
     }, SECRET_SHOW_DURATION_SECONDS * 1000);
 });
 
 $(".secret-close-button").click(function () {
     const secretCloseButton = $(this);
-    const secretCloseId = secretCloseButton.data("secret-id");
-    const secretButton = $(`#${"button-"+secretCloseId}`);
-    const secret = $(`#${secretCloseId}`);
+    const secretId = secretCloseButton.data("secret-id");
+    const secretButton = $(`#${"button-"+secretId}`);
+    const secret = $(`#${secretId}`);
+    hideButtons(secret, secretButton);
+    clearTimeout(timer)
+});
+
+function hideButtons(secret, secretButton) {
     secret.addClass("display-none");
     secretButton.removeClass("display-none");
-});
+}
