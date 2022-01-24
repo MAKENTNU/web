0423840ce9330b06e782bf778bd8f38b839052e7
Dabble <ddabble@users.noreply.github.com>
2022-01-24 06:57:43 +0100

    Made interpolated static files use absolute URLs
    This fixes an issue with some browsers not being able to properly interpret relative URLs, and it also improves PyCharm's code suggestions in
    the interpolated XML files (due to it interpreting the `{% static %}` token as a Django template tag).

--- web/static.py
+++ web/static.py
@@ -1,19 +1,23 @@
+import re
+from fnmatch import fnmatchcase
 from io import BytesIO
-from pathlib import PurePosixPath
+from typing import Dict, List, Tuple
 
+from django.conf import settings
 from django.contrib.staticfiles.storage import HashedFilesMixin, ManifestStaticFilesStorage
-from django.contrib.staticfiles.utils import matches_patterns
 from django.http import FileResponse
-from django.templatetags.static import static
 from django.views.static import serve
 
 
-# In the files matching the glob patterns, the strings matching the regexes are replaced by the inner capturing group,
-# which is first looked up in the static file manifest
+# The glob pattern for the files that should have their contents interpolated (as in https://en.wikipedia.org/wiki/String_interpolation)
+INTERPOLATION_GLOB_PATTERN = '*.interpolated.*'
 INTERPOLATION_PATTERNS = (
-    ('*.interpolated.*', (
+    (INTERPOLATION_GLOB_PATTERN, (
         (
-            r"""(?P<matched>\{% get_relative_static ["'](?P<url>.*?)["'] %\})""",
+            # The strings matching this regex are replaced by the template below
+            r"""(?P<matched>\{% static ["'](?P<url>.*?)["'] %\})""",
+            # This simply inserts the string matched by the `url` capturing group (defined in the regex above) as it is;
+            # when the template is used, the URL in the file has been looked up in the static file manifest, and replaced by its hashed version
             "%(url)s",
         ),
     )),
@@ -22,64 +26,70 @@ INTERPOLATION_PATTERNS = (
 
 class InterpolatingManifestStaticFilesStorage(ManifestStaticFilesStorage):
     """
-    This class extends the functionality of ``ManifestStaticFilesStorage`` by replacing custom ``get_relative_static``
-    tokens in files whose names end with ``.interpolated`` (not including the extension),  with the path of another static file.
+    This class extends the functionality of ``ManifestStaticFilesStorage`` by replacing ``static`` tags
+    in files whose names end with ``.interpolated`` (not including the extension),  with the path of another static file.
 
     For example,
     ::
-        {% get_relative_static './android-chrome-192x192.png' %}
+        {% static 'web/img/favicons/android-chrome-192x192.png' %}
     is replaced by
     ::
-        ./android-chrome-192x192.edcbcb619832.png
-
-    Development note: The paths have to be relative to the file, because of the way ``ManifestStaticFilesStorage`` works.
+        /static/web/img/favicons/android-chrome-192x192.edcbcb619832.png
     """
     patterns = (
         *INTERPOLATION_PATTERNS,
         *ManifestStaticFilesStorage.patterns,
     )
 
+    def url_converter(self, name, *args, **kwargs):
+        base_converter = super().url_converter(name, *args, **kwargs)
+        if not fnmatchcase(name, INTERPOLATION_GLOB_PATTERN):
+            return base_converter
+
+        def converter(match_obj: re.Match):
+            matches = match_obj.groupdict()
+            matched = matches['matched']
+            url = matches['url']
+
+            # Prefix the URL with `STATIC_URL`, so that it doesn't get ignored by `base_converter`
+            # (see https://github.com/django/django/blob/dc8bb35e39388d41b1f38b6c5d0181224e075f16/django/contrib/staticfiles/storage.py#L184-L187)
+            replaced_matched = matched.replace(url, self.get_full_static_url(url))
+            return base_converter(match_obj.re.match(replaced_matched))
+
+        return converter
+
+    @staticmethod
+    def get_full_static_url(url: str):
+        return f"{settings.STATIC_URL}{url}"
+
 
 class _PureInterpolatingFilesMixin(HashedFilesMixin):
     patterns = INTERPOLATION_PATTERNS
 
 
-_compiled_interpolation_patterns = _PureInterpolatingFilesMixin()._patterns
+_compiled_interpolation_patterns: Dict[str, List[Tuple[re.Pattern, str]]] = _PureInterpolatingFilesMixin()._patterns
 
 
-def serve_interpolated(request, path, document_root=None, show_indexes=False):
+def serve_interpolated(request, path, *args, **kwargs):
     """
     This view extends the functionality of Django's ``serve()`` with the same as ``InterpolatingManifestStaticFilesStorage`` does.
     """
-    response = serve(request, path, document_root=document_root, show_indexes=show_indexes)
-    if (isinstance(response, FileResponse) and request.path.startswith("/static")
-            and matches_patterns(path, _compiled_interpolation_patterns)):
+    response = serve(request, path, *args, **kwargs)
+    if (isinstance(response, FileResponse) and request.path.startswith(settings.STATIC_URL)
+            and fnmatchcase(path, INTERPOLATION_GLOB_PATTERN)):
 
-        requested_path_static_dir = PurePosixPath(path).parent
         content = response.getvalue().decode()
 
         for extension, patterns in _compiled_interpolation_patterns.items():
             for pattern, template in patterns:
 
-                def replace(match_obj):
-                    _tag, relative_static_path = match_obj.groups()
-                    registered_static_path = static(str(requested_path_static_dir / relative_static_path))
-                    registered_relative_static_path = replace_filename(
-                        relative_static_path, PurePosixPath(registered_static_path).name
-                    )
-                    return template % {'url': registered_relative_static_path}
+                def replace(match_obj: re.Match):
+                    matches = match_obj.groupdict()
+                    matches['url'] = InterpolatingManifestStaticFilesStorage.get_full_static_url(matches['url'])
+                    return template % matches
 
                 content = pattern.sub(replace, content)
 
         response.streaming_content = BytesIO(content.encode())
 
     return response
-
-
-def replace_filename(path: str, new_filename: str):
-    """
-    Replaces the filename of the given ``path`` with ``new_filename``,
-    keeping any relative path prefix (like ``./``).
-    """
-    directory, _filename, suffix = path.rpartition(PurePosixPath(path).name)
-    return f"{directory}{new_filename}{suffix}"
