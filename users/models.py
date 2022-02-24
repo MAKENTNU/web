efe2e28513711394b7bea993011b9068e6e7e200
Dabble <ddabble@users.noreply.github.com>
2022-02-24 20:58:45 +0100

    Moved code from User.add_perms() to auth_utils.py

--- users/models.py
+++ users/models.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 from card.modelfields import CardNumberField
-from util.auth_utils import perm_to_str
+from util.auth_utils import get_perms
 
 
 class User(AbstractUser):
@@ -33,25 +32,5 @@ class User(AbstractUser):
         return any(self.has_perm(f'{app_label}.{action}_{model_name}') for action in ('add', 'change', 'delete'))
 
     def add_perms(self, *app_labels_and_codenames: str):
-        query = Q()
-        for app_label_and_codename in app_labels_and_codenames:
-            app_label, codename = app_label_and_codename.split('.', 1)
-            query |= Q(content_type__app_label=app_label, codename=codename)
-        perms = Permission.objects.filter(query)
-        self._check_perms_to_add(perms, app_labels_and_codenames)
+        perms = get_perms(*app_labels_and_codenames)
         self.user_permissions.add(*perms)
-
-    @staticmethod
-    def _check_perms_to_add(filtered_perms, perm_strings_to_add):
-        if filtered_perms.count() != len(perm_strings_to_add):
-            perm_strings_to_add_set = set(perm_strings_to_add)
-            if len(perm_strings_to_add_set) != len(perm_strings_to_add):
-                raise ValueError("The permission arguments provided contain duplicates")
-
-            perms_not_found = perm_strings_to_add_set - {perm_to_str(perm) for perm in filtered_perms}
-            if not perms_not_found:
-                # If all the perm arguments were found, it means that the filtered perms contain duplicate
-                # combinations of app labels and codenames, in which case it's fine to just add the duplicates
-                return
-            perms_not_found_str = ", ".join(f"'{perm}'" for perm in perms_not_found)
-            raise Permission.DoesNotExist(f"The following permissions do not exist: {perms_not_found_str}")
--- util/auth_utils.py
+++ util/auth_utils.py
@@ -1,10 +1,37 @@
+from typing import Tuple
+
 from django.contrib.auth.models import Permission
+from django.db.models import Q, QuerySet
 
 
-def get_perm(app_label_and_codename: str):
+def get_perms(*app_labels_and_codenames: str) -> QuerySet[Permission]:
+    query = Q()
+    for app_label_and_codename in app_labels_and_codenames:
+        app_label, codename = app_label_and_codename.split('.', 1)
+        query |= Q(content_type__app_label=app_label, codename=codename)
+    perms = Permission.objects.filter(query)
+    _check_perms(perms, app_labels_and_codenames)
+    return perms
+
+
+def _check_perms(filtered_perms: QuerySet[Permission], perm_strings: Tuple[str, ...]):
+    if filtered_perms.count() != len(perm_strings):
+        perm_strings_set = set(perm_strings)
+        if len(perm_strings_set) != len(perm_strings):
+            raise ValueError("The permission arguments provided contain duplicates")
+
+        perms_not_found = perm_strings_set - {perm_to_str(perm) for perm in filtered_perms}
+        if not perms_not_found:
+            # If all the perm arguments were found, it means that the filtered perms contain duplicate
+            # combinations of app labels and codenames, in which case it's fine to just return them
+            return
+        perms_not_found_str = ", ".join(f"'{perm}'" for perm in perms_not_found)
+        raise Permission.DoesNotExist(f"The following permissions do not exist: {perms_not_found_str}")
+
+
+def get_perm(app_label_and_codename: str) -> Permission:
     """Find the permission object for an <app_label>.<codename> string."""
-    app_label, codename = app_label_and_codename.split('.', 1)
-    return Permission.objects.get(content_type__app_label=app_label, codename=codename)
+    return get_perms(app_label_and_codename).get()
 
 
 def perm_to_str(permission: Permission) -> str:
