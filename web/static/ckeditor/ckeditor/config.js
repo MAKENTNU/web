68012913c03308871d3a2897591a46e9ae2ac182
Dabble <ddabble@users.noreply.github.com>
2022-02-25 03:07:53 +0100

    Changed layout of the CKEditor toolbar
    * Reordered and added some buttons
    * Removed the "Address" and "Normal (DIV)" options of the "Paragraph Format" dropdown
      * (Or, rather, set the dropdown options to only be "Normal", "Heading 1" to "Heading 6", and "Formatted"; see `config.format_tags` in `config.js`)

--- web/static/ckeditor/ckeditor/config.js
+++ web/static/ckeditor/ckeditor/config.js
@@ -1,11 +1,20 @@
 /*
   Note: To make this configuration file apply, the `ckeditor` app must be listed after `web` in `INSTALLED_APPS`.
 */
+const customStylesName = "my_styles";
+CKEDITOR.stylesSet.add(customStylesName, [
+    {name: gettext("Big"), element: "big"},
+    {name: gettext("Small"), element: "small"},
+    {name: gettext("Code"), element: "code"},
+    {name: gettext("Quotation"), element: "q"},
+]);
 
 CKEDITOR.editorConfig = function (config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+    config.format_tags = "p;h1;h2;h3;h4;h5;h6;pre";
+    config.stylesSet = customStylesName;
 
     if (CKEDITOR_CONFIG_FROM_DJANGO.shouldAllowAllTags) {
         // Code based on https://stackoverflow.com/a/24575744
