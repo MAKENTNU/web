b49f4f6d92ea9bacae5ded7acfcce1fb93016616
Dabble <ddabble@users.noreply.github.com>
2022-02-27 07:53:48 +0100

    Disable the whole stream_name field
    ...instead of just the `<input>` tag.

--- make_queue/static/make_queue/js/machine_create.js
+++ make_queue/static/make_queue/js/machine_create.js
@@ -1,8 +1,8 @@
-const $streamNameInput = $("#id_stream_name");
+const $streamNameField = $("#id_stream_name").closest(".field");
 
 // Disable the stream name field if the selected machine type does not support streams
 $("select#id_machine_type").change(function () {
     const $selectedMachineType = $(this).find(":selected");
     const hasStream = $selectedMachineType.data("has-stream") !== undefined;
-    $streamNameInput.prop("disabled", !hasStream);
+    $streamNameField.toggleClass("disabled", !hasStream);
 });
