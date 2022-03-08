9e8e65f6ddfb4a1f4d7617a1c93111ba4616c037
Dabble <ddabble@users.noreply.github.com>
2022-03-08 16:32:31 +0100

    Fixed only one language in content box forms
    This could be reproduced by editing the internal content box, and then - without restarting the server - editing a content box on the main site
    (e.g. the "about" content box), which would now also only have one language.
    
    (For the record, I was not able to reproduce this bug on my machine, but three other members were able to.)

--- contentbox/forms.py
+++ contentbox/forms.py
@@ -10,19 +10,19 @@ class ContentBoxForm(forms.ModelForm):
     # The expected names of the subwidgets (one for each language) of `MultiLingualFormField`
     CONTENT_SUBWIDGET_NAMES = [f'content_{language}' for language in MultiLingualTextStructure.SUPPORTED_LANGUAGES]
 
-    content_extra_widget_kwargs = {}
-
     class Meta:
         model = ContentBox
         fields = ('content',)
 
-    def __init__(self, *args, single_language: str = False, **kwargs):
+    def __init__(self, *args, single_language: str = False, content_extra_widget_kwargs: dict = None, **kwargs):
         """
         :param single_language: A language code;
                                 if set, the form widget of ``content`` will display only this language, and the submitted value for this language
                                 will be copied to the other languages used by the website.
+        :param content_extra_widget_kwargs: Extra kwargs for the widget of the ``content`` field.
         """
         self.single_language = single_language
+        self.content_extra_widget_kwargs = content_extra_widget_kwargs or {}
         if self.single_language:
             self.content_extra_widget_kwargs['languages'] = [self.single_language]
 
@@ -53,8 +53,13 @@ class EditSourceContentBoxForm(ContentBoxForm):
 
     NOTE: Make sure that the user has the ``internal.can_change_rich_text_source`` permission before using this form.
     """
-    content_extra_widget_kwargs = {
-        'subwidget_kwargs': {
-            'config_name': settings.CKEDITOR_EDIT_SOURCE_CONFIG_NAME,
-        },
-    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **{
+            'content_extra_widget_kwargs': {
+                'subwidget_kwargs': {
+                    'config_name': settings.CKEDITOR_EDIT_SOURCE_CONFIG_NAME,
+                },
+            },
+            **kwargs,
+        })
