b64ded3f612bd476a1dbb977094a4fe71c999cdb
Dabble <ddabble@users.noreply.github.com>
2022-03-24 02:30:12 +0100

    Fixed 404 for CreateDocumentationPageView
    This was caused by the `create` part of request URLs being matched by the `<PageTitle:title>` path parameter of `DocumentationPageDetailView`.

--- docs/urls.py
+++ docs/urls.py
@@ -12,11 +12,11 @@ register_converter(converters.SpecificPageByTitle, 'PageTitle')
 
 unsafe_urlpatterns = [
     path("", views.DocumentationPageDetailView.as_view(), {'title': Page.objects.get_or_create(title=MAIN_PAGE_TITLE)[0].title}, name='home'),
+    path("page/create/", views.CreateDocumentationPageView.as_view(), name='create_page'),
     path("page/<PageTitle:title>/", views.DocumentationPageDetailView.as_view(), name='page_detail'),
     path("page/<PageTitle:title>/history/", views.HistoryDocumentationPageView.as_view(), name='page_history'),
     path("page/<PageTitle:title>/history/change/", views.ChangeDocumentationPageVersionView.as_view(), name='change_page_version'),
     path("page/<PageTitle:title>/history/<int:content_pk>/", views.OldDocumentationPageContentView.as_view(), name='old_page_content'),
-    path("page/create/", views.CreateDocumentationPageView.as_view(), name='create_page'),
     path("page/<PageTitle:title>/edit/", views.EditDocumentationPageView.as_view(), name='edit_page'),
     path("page/<PageTitle:title>/delete/", views.DeleteDocumentationPageView.as_view(), name='delete_page'),
     path("search/", views.SearchPagesView.as_view(), name='search_pages'),
