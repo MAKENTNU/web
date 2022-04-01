ed58a971e976f77c8944319d1c47fc81434b1871
Dabble <ddabble@users.noreply.github.com>
2022-04-01 18:37:36 +0200

    Improved QuoteAdmin
    Also fixed an exception when searching, as it's not valid to include a foreign key field (`author`) in `search_fields`, without specifying fields on
    the related model using lookup syntax (double underscores). This is now done in `UserSearchFieldsMixin`.

--- internal/admin.py
+++ internal/admin.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from simple_history.admin import SimpleHistoryAdmin
 
 from util.admin_utils import DefaultAdminWidgetsMixin, UserSearchFieldsMixin, search_escaped_and_unescaped
-from .models import Member, Secret, SystemAccess, Quote
+from .models import Member, Quote, Secret, SystemAccess
 
 
 class MemberAdmin(DefaultAdminWidgetsMixin, admin.ModelAdmin):
@@ -49,9 +49,20 @@ class SecretAdmin(DefaultAdminWidgetsMixin, SimpleHistoryAdmin):
         return search_escaped_and_unescaped(super(), request, queryset, search_term)
 
 
-class QuoteAdmin(DefaultAdminWidgetsMixin, admin.ModelAdmin):
+class QuoteAdmin(DefaultAdminWidgetsMixin, UserSearchFieldsMixin, admin.ModelAdmin):
     list_display = ('quote', 'quoted', 'author')
-    search_fields = ('quote', 'quoted', 'author')
+    list_filter = (
+        'quoted',
+        ('author', admin.RelatedOnlyFieldListFilter),
+    )
+    search_fields = (
+        'quote', 'quoted', 'context',
+        # The user search fields are appended in `UserSearchFieldsMixin`
+    )
+    user_lookup, name_for_full_name_lookup = 'author__', 'author_full_name'
+    list_select_related = ('author',)
+
+    autocomplete_fields = ('author',)
 
 
 admin.site.register(Member, MemberAdmin)
