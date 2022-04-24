8e2dd0e7d3c0630dde7e67ac5e2e06e1361f0b25
Dabble <ddabble@users.noreply.github.com>
2022-04-24 22:36:31 +0200

    Split common_utils.js into two files
    Keeping function definitions in `common_utils.js` while linking it without the `defer` keyword, makes it easier to use those functions in other
    parts of the code that's not necessarily parsed after `common_utils.js` is when deferred.
    
    The code in `common_deferred_code.js`, however, should always deferred, as the code it contains shouldn't be executed until after the DOM is ready.

--- web/static/web/js/common_utils.js
+++ web/static/web/js/common_utils.js
@@ -1,17 +1,3 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-$(".message .close").click(function () {
-    $(this)
-        .closest(".message")
-        .fadeOut();
-});
-
-$("span[data-content], .explanation-popup").popup();
-
-// Only forms that have not opted out (using the `dont-prevent-leaving` class),
-// and that have a `method` attribute that is not `GET` (case-insensitive) - as those forms shouldn't contain data that is saved in the backend
-$("form:not(.dont-prevent-leaving)[method]:not([method=GET])").dirty({
-    preventLeaving: true,
-});
