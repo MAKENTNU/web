8c7c6e5f535d914fc345d47d826515eb75cb567f
Dabble <ddabble@users.noreply.github.com>
2022-05-16 16:11:35 +0200

    Fixed error when reducing TemporaryUploadedFile
    Uploaded files are of the type `TemporaryUploadedFile` when larger than 2.5 MB
    (see https://docs.djangoproject.com/en/4.0/ref/settings/#std:setting-FILE_UPLOAD_MAX_MEMORY_SIZE),
    which were both closed too early in `file_utils.calculate_file_hash()` and not handled properly in `CompressedImageField.clean()`.

--- util/formfields.py
+++ util/formfields.py
@@ -4,7 +4,8 @@ from typing import Union
 
 from PIL import Image
 from django import forms
-from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files import File
+from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
 from django.db.models.fields.files import ImageFieldFile
 
 from util.file_utils import file_contents_equal, filenames_equal
@@ -18,7 +19,7 @@ class CompressedImageField(forms.ImageField):
     """
 
     def clean(self, data: Union[InMemoryUploadedFile, bool, None], initial: ImageFieldFile = None):
-        cleaned_data: Union[ImageFieldFile, InMemoryUploadedFile, bool, None] = super().clean(data, initial=initial)
+        cleaned_data: Union[ImageFieldFile, InMemoryUploadedFile, TemporaryUploadedFile, bool, None] = super().clean(data, initial=initial)
         if data and cleaned_data:
             if initial and file_contents_equal(cleaned_data, initial):
                 if not filenames_equal(cleaned_data, initial):
@@ -30,15 +31,29 @@ class CompressedImageField(forms.ImageField):
             try:
                 with Image.open(cleaned_data) as pillow_image:
                     if pillow_image.format == 'JPEG':
-                        output = BytesIO()
-                        pillow_image.save(output, format='JPEG', quality=90)
-                        output.seek(0)
 
-                        return InMemoryUploadedFile(
-                            output, cleaned_data.field_name, cleaned_data.name, cleaned_data.content_type, sys.getsizeof(output), None,
-                        )
+                        if isinstance(cleaned_data, InMemoryUploadedFile):
+                            output = BytesIO()
+                            self._save_reduced_image(pillow_image, output)
+                            return InMemoryUploadedFile(
+                                output, cleaned_data.field_name, cleaned_data.name,
+                                cleaned_data.content_type, sys.getsizeof(output), cleaned_data.charset,
+                            )
+                        elif isinstance(cleaned_data, TemporaryUploadedFile):
+                            new_file = TemporaryUploadedFile(
+                                cleaned_data.name, cleaned_data.content_type, 0, cleaned_data.charset, cleaned_data.content_type_extra,
+                            )
+                            self._save_reduced_image(pillow_image, new_file)
+                            return new_file
+                        else:
+                            raise forms.ValidationError(f"Unexpected type of uploaded file: {type(cleaned_data)}")
+
             except IOError as e:
                 # Pillow (PIL) will throw an IOError if it cannot open the image, or does not support the given format
                 get_request_logger().exception(e)
 
         return cleaned_data
+
+    @staticmethod
+    def _save_reduced_image(image: Image, file: Union[BytesIO, File]):
+        image.save(file, format='JPEG', quality=90)
