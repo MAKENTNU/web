db588d23f9cdd3e3432c77f474eda5adfeab404f
Dabble <ddabble@users.noreply.github.com>
2022-07-16 19:21:38 +0200

    Optimized stream viewing on machine list page
    Now, only the visible streams are connected to;
    once the page is scrolled so that a stream image is no longer rendered, the stream for that machine is disconnected.
    
    The machine detail page does not adopt this new behavior, as it's not really necessary when there is always only one stream on the page.

--- make_queue/static/make_queue/js/stream.js
+++ make_queue/static/make_queue/js/stream.js
@@ -1,4 +1,8 @@
+/* `connectStreamsOutsideViewport` can optionally be defined when linking this script */
+var connectStreamsOutsideViewport;
 const STREAM_INIT_TIMEOUT_SECONDS = 10;
+const streamNameToData = {};
+const streamNameToOriginalImg = {};
 
 function getStreamName($streamImage) {
     return $streamImage.data("stream-name");
@@ -23,10 +27,52 @@ function initStream($streamImage) {
         clearTimeout(timeoutID);
     };
     newImage.src = `/reservation/machines/${streamName}/stream/`; // URL defined in the server's Nginx config
+
+    return {newImage, timeoutID};
 }
 
+function uninitStream($streamImage) {
+    const streamName = getStreamName($streamImage);
+    const streamData = streamNameToData[streamName];
+    // Stream hasn't been initialized for this element yet (usually happens on page load)
+    if (!streamData)
+        return;
+
+    const {newImage, timeoutID} = streamData;
+    clearTimeout(timeoutID);
+    newImage.onload = undefined;
+    newImage.onerror = undefined;
+    newImage.src = "";
+    $streamImage.attr("src", streamNameToOriginalImg[streamName]);
+}
+
+// Code based on https://stackoverflow.com/a/45618188
+const observer = new IntersectionObserver(
+    function (entries, opts) {
+        entries.forEach(function (entry) {
+            const $streamImage = $(entry.target);
+            // Only connect (and stay connected) to streams while they're visible
+            if (entry.isIntersecting) {
+                const streamData = initStream($streamImage);
+                streamNameToData[getStreamName($streamImage)] = streamData;
+            } else
+                uninitStream($streamImage);
+        });
+    }, {
+        root: null, // makes it observe intersections between the viewport and the observed elements
+        threshold: 0, // triggers the callback above just as the observed element comes into / out of view
+    },
+);
+
 $(".stream.image").each(function () {
-    initStream($(this));
+    const $streamImage = $(this);
+    const streamName = getStreamName($streamImage);
+    streamNameToOriginalImg[streamName] = $streamImage.attr("src");
+
+    if (connectStreamsOutsideViewport)
+        initStream($streamImage);
+    else
+        observer.observe(this);
 }).click(function () {
     $(this).toggleClass("fullscreen");
     $("#fader, #close-fullscreen-button").toggleClass("fullscreen");
