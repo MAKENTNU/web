7830c20550af0bc30f25c873e0198c62152977a0
Anders <6058745+ddabble@users.noreply.github.com>
2023-02-23 23:34:42 +0100

    Updated Dataporten social config
    This was prompted by an email from Feide on November 25th, 2022 titled "Feide - Change notification for https://auth.dataporten.no",
    which notified us of an upcoming deprecation of the `/userinfo` endpoint (used in
    https://github.com/MAKENTNU/web/blob/3a5e5eba961e123c83c1380f8cb653135da5c8d9/dataporten/social.py#L56);
    see https://docs.feide.no/reference/oauth_oidc/legacy_userinfo.html for some additional details.
    This motivated me to see if more things could be update in the same file, which in total turned out like this:
    * Made `DataportenOAuth2` extend `OpenIdConnectAuth` (which extends `BaseOAuth2`), to make the class more specialized for our case, since we use
      OpenIdConnect
    * Removed overriding `ID_KEY`, as the new userinfo endpoint (see the bullet point below on `user_data()`) uses the same value for this variable as
      `OpenIdConnectAuth` has already set
    * Removed overriding `REQUIRES_EMAIL_VALIDATION`, as its default value is the same
    * Removed `userid` from `EXTRA_DATA`, as that claim is no longer in the response from Dataporten
    * Added overriding `OIDC_ENDPOINT`, as it's required by `OpenIdConnectAuth`
    * Added some attribute groups to the superclass' `DEFAULT_SCOPE`, to request the necessary info for registering users
    * Removed `BASE_URL`, as it's replaced by `OIDC_ENDPOINT`
    * Removed `API_URL`, as it was unused
    * Removed overriding `AUTHORIZATION_URL`, as the super class' `authorization_url()` automatically retrieves this URL from a "well-known" API
      (https://auth.dataporten.no/.well-known/openid-configuration)
    * Removed overriding `ACCESS_TOKEN_METHOD`, as its default value is the same
    * Removed overriding `ACCESS_TOKEN_URL`, for the same reason as for `AUTHORIZATION_URL` above
    * Removed overriding `STATE_PARAMETER`, as its default value is the same
    * Removed overriding `REDIRECT_STATE`, as its default value is the same
    * Removed overriding `auth_complete_credentials()`, as its return value is same in the super class' implementation
    * Added `USERID_SEC_CLAIM_NAME` - a custom setting used in `get_user_details()` in place of the `'userid_sec'` string, as this setting contains the
      new key for the secondary user ID (containing mainly the Feide username) from the response from Dataporten
    * Made `get_user_details()` return the dict from its super implementation, instead of just the `response` parameter, which is what `OpenIdConnectAuth`
      expects
    * Removed overriding `check_correct_audience()`, as the social library already automatically validates the audience through the
      `OpenIdConnectAuth.request_access_token()` method (when decoding the JWT (JSON Web Token) `id_token`)
    * Removed overriding `user_data()`, for two main reasons:
      1. The implementation in the super class handles automatically retrieving the correct endpoint to use, through the "well-known" API mentioned above
         - which currently returns `/openid/userinfo`
      2. We don't have to validate the audience anymore - see the previous bullet point on `check_correct_audience()`
    
    Also added a requirement to `social-auth-core[openidconnect]` to be able to use OpenIdConnect
    (see the docs linked to on https://pypi.org/project/social-auth-app-django/:
    https://python-social-auth.readthedocs.io/en/stable/installing.html#using-the-extras-options).

--- dataporten/social.py
+++ dataporten/social.py
@@ -1,42 +1,41 @@
-from social_core.backends.oauth import BaseOAuth2
-from social_core.exceptions import AuthException
+from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 
 # Code based on https://github.com/Uninett/python-dataporten-auth/blob/bad1b95483c5da7d279df4a8d542a3c24c928095/src/dataporten/social.py
-class DataportenOAuth2(BaseOAuth2):
+class DataportenOAuth2(OpenIdConnectAuth):
     name = 'dataporten'
-    ID_KEY = 'userid'
-    REQUIRES_EMAIL_VALIDATION = False
-    EXTRA_DATA = [
-        # (<name returned by `get_user_details()`>, <alias for storage in the database - through `UserSocialAuth.extra_data`>)
-        ('scope', 'scope'),
-        ('userid', 'userid'),
-        ('username', 'username'),  # used as the base username for new users; it's set in `get_user_details()` below
-        ('email', 'email'),
+    OIDC_ENDPOINT = 'https://auth.dataporten.no'
+    DEFAULT_SCOPE = OpenIdConnectAuth.DEFAULT_SCOPE + [
+        # These attribute groups (among others) are selected in the "User information" tab of our website's service in Feide's customer portal
+        'userinfo-name',  # From the customer portal: "Given name, surname, legal name, common name and display name"
+        'userid-feide',  # From the customer portal: "User name, personal Feide ID at organization and previous Feide IDs at organization"
+    ]
+    # Names of extra claims (keys in the response dict from Dataporten) to store in the database
+    # - through the `extra_data` field on the `UserSocialAuth` model (e.g. `user.social_auth.first().extra_data`).
+    # If a list element is a tuple, it's interpreted like this:
+    # (<claim name - returned by `get_user_details()`>, <key for the JSON key/value pair stored through `extra_data`>)
+    EXTRA_DATA = OpenIdConnectAuth.EXTRA_DATA + [
+        'scope',
+        'username',  # used as the base username for new users; it's set in `get_user_details()` below
+        'email',
         ('name', 'fullname'),
     ]
-    BASE_URL = 'https://auth.dataporten.no'
-    API_URL = 'https://api.dataporten.no'
-    AUTHORIZATION_URL = f'{BASE_URL}/oauth/authorization'
-    ACCESS_TOKEN_METHOD = 'POST'
-    ACCESS_TOKEN_URL = f'{BASE_URL}/oauth/token'
-    STATE_PARAMETER = True
-    REDIRECT_STATE = False
-
-    def auth_complete_credentials(self):
-        return self.get_key_and_secret()
+    # Name of the claim containing the secondary user ID
+    # (see https://docs.feide.no/reference/oauth_oidc/openid_connect_details.html#id-token)
+    USERID_SEC_CLAIM_NAME = 'https://n.feide.no/claims/userid_sec'
 
     def get_user_details(self, response):
         """
-        Converts response data from the format of Dataporten's API, to the format expected by the ``social`` libraries -
-        which is stored in ``UserSocialAuth.extra_data`` (through ``EXTRA_DATA`` above).
+        Converts response data from the format of Dataporten's API, to the format used by the rest of the code.
+
+        See info on the API endpoint here: https://docs.feide.no/reference/apis/userinfo.html
 
         :return: user details from Dataporten.
         """
-        user = response
+        user = super().get_user_details(response)
 
         # Based on https://github.com/Uninett/python-dataporten-auth/blob/bad1b95483c5da7d279df4a8d542a3c24c928095/src/dataporten/social.py#L102-L107
-        for userid in user['userid_sec']:
+        for userid in response[self.USERID_SEC_CLAIM_NAME]:
             usertype, complete_username = userid.split(':')
             if usertype == 'feide':
                 username, _domain = complete_username.split('@')
@@ -44,24 +43,6 @@ class DataportenOAuth2(BaseOAuth2):
                 break
 
         return user
-
-    def check_correct_audience(self, audience):
-        """Assert that Dataporten sends back our own client ID as audience."""
-        client_id, _ = self.get_key_and_secret()
-        if audience != client_id:
-            raise AuthException("Wrong audience")
-
-    def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service."""
-        url = f'{self.BASE_URL}/userinfo'
-        response = self.get_json(
-            url,
-            headers={'Authorization': f'Bearer {access_token}'},
-        )
-        self.check_correct_audience(response['audience'])
-
-        userdata = response['user']
-        return userdata
 
     def refresh_token(self, *args, **kwargs):
         raise NotImplementedError(
