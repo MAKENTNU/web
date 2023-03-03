143baa3cacaf71cb83766a061a7b0a179df9b185
Anders <6058745+ddabble@users.noreply.github.com>
2023-03-03 04:35:07 +0100

    Fixed not loading machine page when using keyboard
    ...to select a different machine in the dropdown on a machine detail page.

--- make_queue/static/make_queue/js/machine_detail.js
+++ make_queue/static/make_queue/js/machine_detail.js
@@ -9,4 +9,11 @@ const calendar = new ReservationCalendar($(".reservation-calendar"), calendarPro
 
 $(".action.list .ui.dropdown.item").dropdown({
     showOnFocus: true,
+    action: function (text, value, $selectedElement) {
+        // Clicking any machine in the dropdown will automatically load the linked page (since each dropdown element is an <a> tag)
+        // but when using the keyboard to select a dropdown element, nothing happens.
+        // This code fixes that by opening the linked page:
+        const linkedPage = $selectedElement.attr("href");
+        window.open(linkedPage, "_self");
+    },
 });
