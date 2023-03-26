1b4685eb2a9b668724b706727979f900799d461a
Anders <6058745+ddabble@users.noreply.github.com>
2023-03-26 23:00:36 +0200

    Made makemessages convert Windows file path format
    ...of the file location comments to the format generated on Linux
    (forward slashes (/) instead of backslashes (\) and no leading `.\`).

--- src/web/management/commands/makemessages.py
+++ src/web/management/commands/makemessages.py
@@ -1,3 +1,6 @@
+import re
+from pathlib import Path
+
 from django.conf import settings
 from django.core.management.commands import makemessages
 
@@ -9,6 +12,10 @@ class Command(makemessages.Command):
         " except for the Django source directory (the 'BASE_DIR' setting)."
     )
 
+    # Remove leading `./` as well, in case it ever happens to be generated (see comment above `write_po_file()` for context)
+    _path_prefix_regex = re.compile(r" \.[\\/]")
+    _path_prefix_replacement = " "
+
     def execute(self, *args, **options):
         ignore_patterns = options['ignore_patterns']
         if not ignore_patterns:
@@ -18,3 +25,33 @@ class Command(makemessages.Command):
                     ignore_patterns.append(path.name)
 
         return super().execute(*args, **options)
+
+    # On Windows, it seems like the `.po` files consistently have their file location comments formatted with
+    # backslashes (\) instead of forward slashes (/) - which is the standard file path format on Windows - and
+    # a leading `.\` path prefix.
+    # The code below converts those comments to the format generated on Linux (forward slashes and no leading `.\`),
+    # to ensure that the command output is not dependent on each developer's operating system.
+    def write_po_file(self, potfile, locale):
+        super().write_po_file(potfile=potfile, locale=locale)
+
+        # Based on https://github.com/django/django/blob/4.1.7/django/core/management/commands/makemessages.py#L683-L685
+        locale_dir = Path(potfile).parent / locale / 'LC_MESSAGES'
+        po_file = locale_dir / f'{self.domain}.po'
+
+        original_po_file_contents = po_file.read_text(encoding='utf-8')
+        changed = False
+        po_file_lines = original_po_file_contents.splitlines(keepends=True)
+        for i, line in enumerate(po_file_lines):
+            if line.startswith("#:"):
+                po_file_lines[i] = (
+                    self._path_prefix_regex.sub(self._path_prefix_replacement, line)
+                    .replace("\\", "/")
+                )
+                changed = True
+
+        # Faster to check this bool than compare the new and old file contents - especially if nothing was changed
+        if changed:
+            new_po_file_contents = "".join(po_file_lines)
+            # Based on https://github.com/django/django/blob/4.1.7/django/core/management/commands/makemessages.py#L707-L708
+            with open(po_file, 'w', encoding='utf-8') as fp:
+                fp.write(new_po_file_contents)
