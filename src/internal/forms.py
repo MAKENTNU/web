17d65c58dc29aa18ba2502440e084e926e006088
Gunvor4 <Gunvor4@users.noreply.github.com>
2023-08-18 10:39:22 +0200

    Made form for lore articles
    Made form to be used when adding or changing lore articles

--- src/internal/forms.py
+++ src/internal/forms.py
@@ -6,7 +6,7 @@ from card import utils as card_utils
 from card.formfields import CardNumberField
 from users.models import User
 from web.widgets import SemanticDateInput, SemanticMultipleSelectInput, SemanticSearchableChoiceInput
-from .models import Member, Quote, Secret, SystemAccess
+from .models import Member, Quote, Secret, SystemAccess, Lore
 
 
 class AddMemberForm(forms.ModelForm):
@@ -176,3 +176,9 @@ class QuoteForm(forms.ModelForm):
         widgets = {
             'date': SemanticDateInput(),
         }
+
+
+class LoreForm(forms.ModelForm):
+    class Meta:
+        model = Lore
+        fields = ('title', 'text', 'image')
