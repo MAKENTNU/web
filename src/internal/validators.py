a430134eae89d57c7e33bffcee8721a10c78220c
Gunvor4 <Gunvor4@users.noreply.github.com>
2024-02-23 23:41:12 +0100

    Changed validator for Discord username
    Discord changed their usernames, and removed the hashtag and the four digits at the end of usernames. Discord usernames now consist of lowercase characters (a-z), numbers (0-9), underscores, and periods. Two consecutive periods are not allowed, and usernames have to be between 2 and 32 characters long. Updated the discord_username_regex accordingly.

--- src/internal/validators.py
+++ src/internal/validators.py
@@ -34,8 +34,8 @@ def validate_semester_float(value):
         )
 
 
-discord_username_regex = re.compile(r'^(.+)#([0-9]{4})$')
+discord_username_regex = re.compile(r'^(?!.*\.\.)[a-z0-9_.]{2,32}$')
 discord_username_validator = RegexValidator(
     discord_username_regex,
-    _("Enter a valid Discord username – including the hashtag and the four digits at the end."),
+    _("Enter a valid Discord username."),
 )
